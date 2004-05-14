782d5b55cfa0566a5be85c0841662c413727016e
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2004-05-14 17:21:43 +0000

    Fixing some line ending problems due to the CVS clean up.

--- trunk/eclass_builder/installer/make_installer.bat
+++ trunk/eclass_builder/installer/make_installer.bat
@@ -1,4 +1,4 @@
 ..\updateVersion.py
 F:\Installer\build.py F:\oss\eclass\eclass_builder\installer\editor.spec
-F:\cx_Freeze-2.2\FreezePython --base-binary ..\cx_Freeze-2.2\Win32GUIbase.exe --install-dir ..\3rdparty\win32\Karrigell --target-name Karrigell.exe F:\oss\eclass\eclass_builder\Karrigell\wxKarrigell.py
+F:\cx_Freeze-2.2\FreezePython --base-binary F:\cx_Freeze-2.2\Win32GUIbase.exe --install-dir ..\3rdparty\win32\Karrigell --target-name Karrigell.exe F:\oss\eclass\eclass_builder\3rdparty\src\Karrigell\wxKarrigell.py
 C:\Progra~1\nsis\makensis eclass-builder.nsi
