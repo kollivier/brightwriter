5d0726c2c50ff8725ba61a0e8a7c050c0e8b8aa5
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2004-12-21 00:30:27 +0000

    Adding source for EClass loader script.

---
+++ eclass_builder/autorun/loader/loader.cpp
@@ -0,0 +1,58 @@
+// loader.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <memory.h>
+#include <shellapi.h>
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
+{
+ 	HKEY regKey;
+	bool hasDM = false;
+
+	if (::RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("Software\\Vaclav Slavik\\Documancer"), 0, KEY_QUERY_VALUE, &regKey) == ERROR_SUCCESS){
+		hasDM = true;
+	}
+	else if (::RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("Software\\Vaclav Slavik\\Documancer"), 0, KEY_QUERY_VALUE, &regKey) == ERROR_SUCCESS){
+		hasDM = true;
+	}
+
+	if (hasDM){
+		ShellExecute(0, "open", "eclass.dmbk", "", "", SW_SHOWMAXIMIZED);
+	}
+	else{
+		SHELLEXECUTEINFO si;
+		memset(&si, 0, sizeof(si));
+		si.cbSize = sizeof(si);
+		si.hwnd = NULL;
+		si.lpVerb = "open";
+		si.lpFile = "installers\\documancer-0.2.4-setup.exe";
+		si.nShow = SW_NORMAL;
+		si.fMask = SEE_MASK_NOCLOSEPROCESS | SEE_MASK_FLAG_NO_UI;
+		bool res = ShellExecuteEx(&si);
+		if (res){
+			WaitForSingleObject(si.hProcess,
+								INFINITE);
+			//now it's installed, try it again
+			ShellExecute(0, "open", "eclass.dmbk", "", "", SW_SHOWMAXIMIZED);
+		}
+		else{
+			int answer;
+			answer = MessageBox(NULL, "This EClass requires Documancer but you do not have it installed on your system.\nWould you like to download and install the software now?\n\nClicking 'Yes' will take you to 'http://documancer.sf.net'.", "Download Documancer?", MB_YESNO); 
+			if (answer == IDYES)
+				ShellExecute(0, "open", "http://documancer.sf.net", "", "", SW_SHOWMAXIMIZED);
+		}
+	}
+
+	return 0;
+}
+
+
+
