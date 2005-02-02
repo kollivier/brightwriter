76d26609257b2d04ab3a803d3f375f5257483deb
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2005-02-02 20:03:45 +0000

    Improved support for the new make_py_dist.py, and basic support for wxWebKitCtrl.

--- trunk/eclass_builder/wxbrowser.py
+++ trunk/eclass_builder/wxbrowser.py
@@ -1,6 +1,33 @@
 from wxPython.wx import *
 import string
 
+browserlist = []
+
+try:
+	from wxPython.mozilla import *
+	browserlist.append("mozilla")
+except:
+	pass 
+
+try: 
+	from wxPython.iewin import *
+	browserlist.append("ie")
+except:
+	pass
+
+try: 
+	from wxPython.html import wxHtmlWindow
+	browserlist.append("htmlwindow")
+except:
+	pass
+
+try: 
+	from wxPython.webkit import *
+	browserlist.append("webkit")
+except:
+	pass
+
+
 class wxBrowser:
 	"""
 	Wrapper for the various HTML engines available in wxPython. It will first try to load
@@ -23,6 +50,10 @@ class wxBrowser:
 		self.browser = None
 		self.currenturl = ""
 		self.engine = ""
+		self.OnPageChanged = lambda x: x
+		self.OnStatusChanged = lambda x,y: x
+		self.OnTitleChanged = lambda x: x
+		self.OnProgress = lambda x: x
 
 		if preferredBrowser != "":
 			if string.lower(preferredBrowser) == "mozilla":
@@ -31,11 +62,17 @@ class wxBrowser:
 			elif string.lower(preferredBrowser) == "ie":
 				if self._LoadIE():
 					return 
+			elif string.lower(preferredBrowser) == "webkit":
+				if self._LoadWebKitWindow():
+					return 
 			elif string.lower(preferredBrowser) == "htmlwindow":
 				if self._LoadHTMLWindow():
 					return 
 
-		#default order - Mozilla, IE, HTMLWindow
+		#default order - WebKit (on Mac), Mozilla, IE, HTMLWindow
+		if self._LoadWebKitWindow():
+			return
+
 		if self._LoadMozilla():
 			return 
 		
@@ -44,6 +81,9 @@ class wxBrowser:
 
 		if self._LoadHTMLWindow():
 			return 
+
+		if self.browser == None:
+			print "Error: No browser could be found."
 
 		return None #should never happen, but...
 
@@ -84,30 +124,77 @@ class wxBrowser:
 
 	def _LoadMozilla(self):
 		try:
-			from wxPython.mozilla import *
+			global browserlist
+			if not "mozilla" in browserlist:
+				return False
+
 			self.browser = wxMozillaBrowser(self.parent, self.id)
 			self.engine = "mozilla"
+
+			def __OnUrlChanged(self, event):
+				self.currenturl = event.GetNewURL()
+				self.OnPageChanged(self.currenturl) 
+
+			def __OnStatusChanged(self, event):
+				self.OnStatusChanged(event.GetStatusText(), event.IsBusy())
+
+			def __OnTitleChanged(self, event):
+				self.OnTitleChanged(event.GetTitle())
+
+			def __OnProgress(self, event):
+				if event.GetTotalMaxProgress() != -1:
+					self.OnProgress(float(event.GetTotalCurrentProgress()) /
+									float(event.GetTotalMaxProgress()))
+				elif event.GetSelfMaxProgress() != -1:
+					self.OnProgress(float(event.GetSelfCurrentProgress()) /
+									float(event.GetSelfMaxProgress()))
+
+			#EVT_MOZILLA_URL_CHANGED(self.browser, -1, self.__OnUrlChanged)
+			#EVT_MOZILLA_STATUS_CHANGED(self.browser, -1, self.__OnStatusChanged)
+			#EVT_MOZILLA_TITLE_CHANGED(self.browser, -1, self.__OnTitleChanged)
+			#EVT_MOZILLA_PROGRESS(self.browser, -1, self.__OnProgress)
+
 			return True
 		except:
+			if self.browser:
+				self.browser.Destroy()
+
+			import traceback
+			print traceback.print_exc()
 			return False
 
 	def _LoadIE(self):
 		try:
-			if wxPlatform != "__WXMSW__":
+			global browserlist
+			if not "ie" in browserlist:
 				return False
-			else:
-				from wxPython.iewin import *
-				self.browser = wxIEHtmlWin(self.parent, self.id)
-				self.engine = "ie"
-				return True
+
+			self.browser = wxIEHtmlWin(self.parent, self.id)
+			self.engine = "ie"
+			return True
 		except:
 			return False
 
 	def _LoadHTMLWindow(self):
 		try:
-			from wxPython.html import wxHtmlWindow
+			global browserlist
+			if not "htmlwindow" in browserlist:
+				return False
+
 			self.browser = wxHtmlWindow(self.parent, self.id)
 			self.engine = "htmlwindow"
+			return True
+		except:
+			return False
+
+	def _LoadWebKitWindow(self):
+		try:
+			global browserlist
+			if not "webkit" in browserlist:
+				return False 
+
+			self.browser = wxWebKitCtrl(self.parent, self.id)
+			self.engine = "webkit"
 			return True
 		except:
 			return False
