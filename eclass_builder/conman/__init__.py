0742f0e3b0ddeb31215701ff33ed914ae2b6aa4f
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2005-02-08 04:01:24 +0000

    Import all of conman.py when conman is loaded, so that we don't need to access conman.conman module. :-)

--- eclass_builder/conman/__init__.py
+++ eclass_builder/conman/__init__.py
@@ -1,4 +1,4 @@
 """conman - Content Manager and Metadata Storage Utility"""
 __version__ = "0.6"
-import conman
-__all__ = ["conman", "validate", "xml_settings", "file_functions", "HTMLFunctions", "plugins"]
+from conman import *
+__all__ = ["validate", "xml_settings", "file_functions", "HTMLFunctions", "plugins"]
