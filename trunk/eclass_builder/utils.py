cabab40de48778fb0b006ed168fc1f6b03345882
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2005-02-08 03:57:05 +0000

    Added a case where we don't know if read/write error.

--- trunk/eclass_builder/utils.py
+++ trunk/eclass_builder/utils.py
@@ -11,9 +11,11 @@ class LogFile:
 
 def getStdErrorMessage(type = "IOError", args={}):
 	if type == "IOError":
-		if args["type"] == "write":
+		if args.has_key("type") and args["type"] == "write":
 			return _("There was an error writing the file '%(filename)s' to disk. Please check that you have enough hard disk space to write this file and that you have permission to write to the file.") % {"filename":args["filename"]}
-		elif args["type"] == "read":
+		elif args.has_key("type") and args["type"] == "read":
 			return _("Could not read file '%(filename)s from disk. Please check that the file exists in the location specified and that you have permission to open/view the file.") % {"filename":args["filename"]}
+		elif args.has_key("filename"):
+			return _("There was a problem reading or writing the file %(filename)s. Please check that the file exists and that you have correct permissions to the file.") % {"filename":args["filename"]} 
 	elif type == "UnknownError":
 		return _("An unknown error has occurred. A traceback has been written to the 'errlog.txt' file within your program directory.")
