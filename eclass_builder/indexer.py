21949029e8780eb6ac6da400e47e2f398acb0469
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2005-03-01 06:36:23 +0000

    Added support for indexing File directory, and for new indexing progress dialog.

--- eclass_builder/indexer.py
+++ eclass_builder/indexer.py
@@ -2,38 +2,61 @@
 # indexer.py - controls the indexing and searching
 # of Lucene indexes
 #####################################
-import string, os, StringIO, formatter, locale
+import string, os, StringIO, formatter, locale, glob
 import PyLucene
 import converter
 from HTMLParser import HTMLParser, HTMLParseError
+from wxPython.wx import *
+import locale
+import settings
+import dialogs
 
 class SearchEngine:
-	def __init__(self, parent, indexdir):
+	def __init__(self, parent, indexdir, folder=""):
 		self.parent = parent
 		self.indexdir = indexdir
+		self.folder = folder #for indexing non-EClass docs
 		self.writer = None
 		self.publisher = None
+		self.numFiles = 0
+		self.dialog = None
+		self.filecount = 0
+		self.keepgoing = True
 
-		if not os.path.exists(indexdir):
-			os.mkdir(indexdir)
+		#get a tally of all the files being indexed
+		for (dir, subdirs, files) in os.walk(os.path.join(settings.CurrentDir, "pub"), False):
+			self.numFiles = self.numFiles + len(files)
+				
+		for (dir, subdirs, files) in os.walk(os.path.join(settings.CurrentDir, "File"),False):
+			self.numFiles = self.numFiles + len(files)
+
+		#self.dialog = dialogs.ProgressMonitor(_("Updating Index..."), self.numFiles, _("Preparing to update Index...") + "                             ")
 
 	def IndexDoc(self, node):
+		if not self.keepgoing:
+			return
 		doc = PyLucene.Document()
-		statustext = "Indexing page " + node.content.metadata.name
+		self.statustext = _("Indexing page ") + node.content.metadata.name
 		self.publisher = None
 		ext = os.path.splitext(node.content.filename)[1][1:]
-		for plugin in self.parent.parent.myplugins:
+		for plugin in settings.plugins:
 			if ext in plugin["Extension"]:
 				exec("import plugins." + plugin["Name"])
 				self.publisher = eval("plugins." + plugin["Name"] + ".HTMLPublisher()")
-		if self.parent.txtProgress:
-			self.parent.txtProgress.WriteText(statustext + "\n")
+		if self.dialog:
+			wxYield()
+			self.keepgoing = self.dialog.Update(self.filecount, self.statustext)
+			#self.dialog.sendUpdate(self.filecount, self.statustext)
+			#self.cancel = wxCallAfter(self.dialog.Update, self.filecount, statustext)
+		self.filecount = self.filecount + 1
 		doc.add(PyLucene.Field("title", node.content.metadata.name, True, True, True))
 		filename = node.content.filename
 		if self.publisher:
 			filename = "pub/" + self.publisher.GetFilename(node.content.filename)
 		import urllib
 		filename = string.replace(filename, "\\", "/")
+
+		print "Filename: " + filename
 		doc.add(PyLucene.Field("url", filename, True, True, True))
 		doc.add(PyLucene.Field("description", node.content.metadata.description, True, True, True))
 		doc.add(PyLucene.Field("keywords", node.content.metadata.keywords, True, True, True))
@@ -58,72 +81,133 @@ class SearchEngine:
 
 		doc.add(PyLucene.Field("contents", mytext, True, True, True))
 
-		#print "Adding " + node.content.metadata.name + " to index."
 		if self.writer:
 			self.writer.addDocument(doc)
 
 		for child in node.children:
 			self.IndexDoc(child)
 
-	def IndexFiles(self, rootnode):
+	def IndexFolder(self, dir):
+		for afile in glob.glob(os.path.join(dir, "*")):
+			if not self.keepgoing:
+				return 
+			fullname = os.path.join(dir, afile)
+			if os.path.isdir(fullname):
+				self.IndexFolder(fullname)
+			elif os.path.isfile(fullname) and os.path.splitext(fullname)[1][1:] != "xls":
+				doc = PyLucene.Document()
+				filename = string.replace(fullname, self.folder, "File")
+				filename = string.replace(filename, "\\", "/")
+				print "Filename: " + filename
+				doc.add(PyLucene.Field("title", unicode(os.path.basename(fullname)), True, True, True))
+				doc.add(PyLucene.Field.UnIndexed("url", unicode(filename)))
+				self.statustext = _("Indexing File: \n") + filename
+				if self.dialog:
+					wxYield()
+					self.keepgoing = self.dialog.Update(self.filecount, self.statustext)
+				#	self.cancel = wxCallAfter(self.dialog.Update, self.filecount, statustext)
+				self.filecount = self.filecount + 1
+				mytext = ""
+				try: 
+					#unfortunately, sometimes conversion is hit or miss. Worst case, index the doc with
+					#no text.
+					mytext = self.GetTextFromFile(fullname)
+				except:
+					pass
+
+				doc.add(PyLucene.Field.UnStored("contents", mytext))
+				if self.writer:
+					self.writer.addDocument(doc)
+
+	def IndexFiles(self, rootnode, dialog=None):
 		store = PyLucene.FSDirectory.getDirectory(self.indexdir, True)
 		self.writer = PyLucene.IndexWriter(store, PyLucene.StandardAnalyzer(), True)
-		
+		self.dialog = dialog
+
 		#this will index the root node and all child nodes
 		try:
+			if self.folder != "":
+				self.IndexFolder(self.folder)
 			self.IndexDoc(rootnode)
 		except:
 			import traceback
 			print traceback.print_exc()
-		print "Hello world!"
+
 		self.writer.optimize()
 		self.writer.close()
 
-	def GetTextToIndex(self, node):
+	def GetTextFromFile(self, filename=""):
 		"""
 		Here we convert the contents to text for indexing by Lucene.
 		"""
-		filename = ""
 		data = ""
-		if self.publisher:
-			try:
-				self.publisher.Publish(self.parent.parent, node, node.dir)
-				filename = node.content.filename
-				if self.publisher:
-					filename = self.publisher.GetFilename(node.content.filename)
-				data = open(os.path.join(node.dir, "pub", filename), "rb").read()
-			except:
-				import traceback
-				print traceback.print_exc()
-				return ""
+
+		if filename == "":
+			return ""
+
+		ext = string.lower(os.path.splitext(filename)[1][1:])
+		myconverter = None
+
+		returnDataFormat = "html"
+		if ext in ["htm", "html"]:
+			data = open(filename, "rb").read()
 		else:					
 			try:
 				myconverter = converter.DocConverter(self.parent)
-				thefilename = myconverter.ConvertFile(os.path.join(node.dir, node.content.filename), "html")
+				thefilename, returnDataFormat = myconverter.ConvertFile(filename, "unicodeTxt")
+				if thefilename == "":
+					return ""
 				myfile = open(thefilename, "rb")
 				data = myfile.read()
 				myfile.close()
 			except:
-				print "There was an error here."
 				import traceback
 				print traceback.print_exc()
-				#if os.path.exists(thefilename):
-				#	os.remove(thefilename)
+				if os.path.exists(thefilename):
+					os.remove(thefilename)
 				return ""
 
-		convert = TextConverter()
-		convert.feed(data)
-		convert.close()
-		encoding = "iso-8859-1"
-		if convert.encoding != "":
-			encoding = convert.encoding
-		text = convert.text
+		if returnDataFormat == "html":
+			convert = TextConverter()
+			convert.feed(data)
+			convert.close()
+			encoding = "iso-8859-1"
+			if convert.encoding != "":
+				encoding = convert.encoding
+			text = convert.text
 
-		try: 
-			text = convert.text.decode(encoding)
-		except:
-			text = convert.text.decode(locale.getdefaultlocale()[1])
+			try: 
+				text = convert.text.decode(encoding)
+			except:
+				text = convert.text.decode(locale.getdefaultlocale()[1])
+		elif returnDataFormat == "unicodeTxt":
+			text = unicode(data, "utf-8")
+		else:
+			text = unicode(data)
+
 		return text
+
+
+	def GetTextToIndex(self, node):
+		"""
+		Here we get the file for indexing and pass it into GetTextFromFile.
+		"""
+		filename = ""
+		data = ""
+		if self.publisher:
+			if 1:
+				self.publisher.Publish(self.parent, node, node.dir)
+				filename = node.content.filename
+				if self.publisher:
+					filename = self.publisher.GetFilename(node.content.filename)
+			#except:
+			#	import traceback
+			#	print traceback.print_exc()
+			#	return ""
+		else:					
+			filename = os.path.join(node.dir, node.content.filename)
+
+		return self.GetTextFromFile(filename)
 
 	def SearchFiles(self, query):
 		return []
