f6e3d94f7958a16d86d5f9e85bfa1f9b6063e751
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2005-05-02 23:48:57 +0000

    Allow process.py to support other platforms.

--- eclass_builder/process.py
+++ eclass_builder/process.py
@@ -1,6 +1,12 @@
 import os, sys, string
-import win32api
-win32api.SetErrorMode(65001)
+
+try:
+	#attempt to silence errors on Windows
+	import win32api
+	win32api.SetErrorMode(65001)
+except:
+	pass
+	
 if len(sys.argv) < 2:
 	print "Need program arguments to run"
 else:
