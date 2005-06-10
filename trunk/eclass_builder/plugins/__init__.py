88a8ffc96836df8d180685972b38b8ffd286cc88
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2005-06-10 19:38:17 +0000

    Converting plugin code to module functions rather than class functions.

--- trunk/eclass_builder/plugins/__init__.py
+++ trunk/eclass_builder/plugins/__init__.py
@@ -1,37 +1,34 @@
 import string, os, sys
+import settings
 
-class PluginList:
-	def __init__(self, programdir):
-		self.plugins = []
-		self.programdir = programdir
-		self.filename = ""
+pluginList = []
+def LoadPlugins():
+	global pluginList
+	for item in os.listdir(os.path.join(settings.AppDir, "plugins")):
+		if item[-3:] == ".py" and string.find(item, "__init__.py") == -1 and not item[0] == ".":
+			plugin = string.replace(item, ".py", "")
+			exec("import plugins." + plugin)
+			exec("pluginList.append(plugins." + plugin + ")") 
 
-	def LoadPlugins(self):
-		for item in os.listdir(os.path.join(self.programdir, "plugins")):
-			if item[-3:] == ".py" and string.find(item, "__init__.py") == -1 and not item[0] == ".":
-				plugin = string.replace(item, ".py", "")
-				exec("import plugins." + plugin)
-				exec("self.plugins.append(plugins." + plugin + ".plugin_info)") 
-
-		return self.plugins
-
-	def GetPluginForExtension(self, fileext):
-		for plugin in self.plugins:
-			if extension in plugin["Extension"]:
-				return plugin
+def GetPluginForExtension(fileext):
+	global pluginList
+	for plugin in pluginList:
+		if fileext in plugin.plugin_info["Extension"]:
+			return plugin
 		
-		return None
+	return None
 
-	def GetPlugin(self, name):
-		for plugin in self.plugins:
-			if plugin["Name"] == name or plugin["FullName"] == name:
-				return plugin
+def GetPlugin(name):
+	global pluginList
+	for plugin in pluginList:
+		if plugin.plugin_info["Name"] == name or plugin.plugin_info["FullName"] == name:
+			return plugin
 
-		return None
+	return None
 
-	def GetExtensionsForPlugin(self, name):
-		plugin = self.GetPlugin(name)
-		if plugin:
-			return plugin["Extensions"]
+def GetExtensionsForPlugin(name):
+	plugin = self.GetPlugin(name)
+	if plugin:
+		return plugin.plugin_info["Extensions"]
 
-		return []
+	return []
