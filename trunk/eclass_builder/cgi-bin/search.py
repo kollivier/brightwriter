1d390ac490a0b6e392132782a7e4f5b2dae306be
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2005-06-23 19:40:26 +0000

    New search script, now works with Lucene and EClass.Web viewer.

--- trunk/eclass_builder/cgi-bin/search.py
+++ trunk/eclass_builder/cgi-bin/search.py
@@ -3,138 +3,82 @@ import cgitb; cgitb.enable()
 import string
 import sys
 import os
+import PyLucene
 import win32api
-import win32pipe
-        
+#from PyLucene import QueryParser, IndexSearcher, StandardAnalyzer, FSDirectory
+#from PyLucene import VERSION, LUCENE_VERSION
+       
 scriptname = "search.py"
-scriptdir = "cgi-bin"
+scriptdir = os.path.dirname(os.path.abspath(sys.path[0]))
 eclassdir = "" #indicates current directory
 isiis = 0
+numresults_perpage = 20
 
-import ConfigParser
-if os.path.exists('search.cfg'):
-    config = ConfigParser.ConfigParser()
-    config.read(open('search.cfg'))
-    scriptdir = config.get("directories", "scriptdir")
-    eclassdir = config.get("directories", "eclassdir")
-    isiis = config.get("options", "iis_server")
-else:
-    scriptdir = os.path.dirname(sys.argv[0])
-    if not os.path.exists(os.path.join(scriptdir, scriptname)):
-        #we're in the local server...
-        scriptdir = os.path.join(scriptdir, "cgi-bin")
-    if os.name == "nt":
-        scriptdir = win32api.GetShortPathName(scriptdir)
-    eclassdir = "../pub"
+#import ConfigParser
+#if os.path.exists('search.cfg'):
+#    config = ConfigParser.ConfigParser()
+#    config.read(open('search.cfg'))
+#    eclassdir = config.get("directories", "eclassdir")
+#    isiis = config.get("options", "iis_server")
+#else:
+#    eclassdir = "../pub"
 
-form = QUERY
-#print "Content-type: text/html" # identify response as HTML
+form = cgi.FieldStorage()
+print "Content-type: text/html" # identify response as HTML
 
 content = """
-<FORM method=POST>
-<INPUT type=text name=term
 """
-if form.has_key("term"):
-    content = content + "value='" + string.replace(form["term"], "\"", "&quot;") + "'" 
-
-content = content + ">"
-if form.has_key("page"):
-    content = content + "<INPUT name=page type=hidden value=%(page)s>" % {"page":form["page"]}
-content = content + """
-<select name=searchtype>
-    <option name=all>All text</option>
-    <option name=title>Titles</option>
-    <option name=keywords>Keywords</option>
-</select>
-<INPUT type=submit name="Search" value="Search">
-</FORM>
-    """
 
 if form.has_key("term"):
-    content = content + "<hr><h3>Search Results</h3>"
-    #show search results
+    content = content + "<h3>Search Results</h3>"
     import os
-    search = "-w "
-    term = form["term"]
-    if os.name == "nt":
-        #Windows command line needs double-quotes to be escaped
-        term = string.replace(term, "\"", "\\\"")
-        #print term
+    field = "contents"
+
     if form.has_key("searchtype"):
-        if form["searchtype"] == "All text":
-            search = search + term
-        elif form["searchtype"] == "Titles":
-            search = search + term + " -t t"
-        elif form["searchtype"] == "Keywords":
-            search = search + "Keywords=" + term
-        else:
-            search = search + term
-    #print search
-    page = ""
+        if form["searchtype"].value == "Titles":
+            field = "title"
+        elif form["searchtype"].value == "Keywords":
+            field = "keywords"
+
+    startitem = 1
     if form.has_key("page"):
-        #print "page value = " + form["page"]
-        page = "-b " + `(1 + (20 * (int(form["page"]) - 1)))`
-        #print page
+        startitem = 1 + (numresults_perpage * (int(form["page"].value) - 1))
 
-    index = os.path.join(scriptdir, "..", "index.swish-e")
-    swishbin = os.path.join(scriptdir,"swish-e.exe")
-    if not os.path.exists(index):
-        content = content + "<br>Index file " + index + " does not exist."
-    if not os.path.exists(swishbin):
-        content = content + "<br>program " + swishbin + " does not exist."
-    try:
-        command = swishbin + " -d :: -f \"" + index + "\" -m 20 -p Description " + search + " " + page
-        #content = content + command
-        if 0:
-            myout = win32pipe.popen(command, "r")
+    if 1:
+        directory = PyLucene.FSDirectory.getDirectory(os.path.join(scriptdir, "..", "index.lucene"), False)
+        searcher = PyLucene.IndexSearcher(directory)
+        analyzer = PyLucene.StandardAnalyzer()
+        query = PyLucene.QueryParser.parse(form["term"].value, field, analyzer)
+        hits = searcher.search(query)
+
+        if hits.length() == 0:
+            content = content + "Sorry, your search returned no results."
         else:
-            myout = os.popen(command, "r")
-    except:
-        content = content + "Error running swish-e."
-    results = 0
-    while 1:
-        line = myout.readline()
-        if not line:
-            break
-        else:
-            if not string.find(line, "err: no results") == -1:
-                content = content + "Sorry, your search returned no results."
-                break
-            elif not string.find(line, "# Number of hits:") == -1:
-                results = int(string.split(line, ":")[1])
-                content = content + "<p><i>Your search returned %(results)d results.</i></p>" % {"results":results}
-            elif not string.find(line, "#") == 0:
-                #print line
-                values = string.split(line, "::")
+            content = content + "<p><i>Your search returned %(results)d results.</i></p>" % {"results":hits.length()}
+
+        for num in range(startitem, startitem + numresults_perpage):
+            hit = None
+            if num < hits.length():
                 try:
-                    file = values[1]
-                    if string.find(file, ".") == 0:
-                        file = file[2:]
-
-                    title = values[2]
-                    file = "../" + file
-                    description = ""
-                    try: 
-                        description = values[4]
-                    except:
-                        pass
-                    link = "<p><a href=%(file)s target='_blank'>%(title)s</a><br><font size='-1'><i>Ranking: %(percent)d%% &nbsp;&nbsp;Size: %(bytes)s bytes</i></font><br>%(description)s</p>" % {"file":string.replace(file, " ", "%20"), "title":title, "percent":(int(values[0])/10), "bytes":values[3], "description":description}
-                    content = content + link
-                    
+                    hit = hits.doc(num)
                 except:
                     pass
-            #else:
-            #    content = content + str(line)
-                    
-    if results > 20:
-        result = divmod(results, 20)
-        pages = result[0]
-        if not result[1] == 0:
-            pages = pages + 1
-        for page in range(1, pages + 1):
-            content = content + "[ <a href=/cgi-bin/%(scriptname)s?searchtype=%(searchtype)s&amp;term=%(term)s&amp;page=%(page)d>%(page)d</a> ]" % {"scriptname": scriptname, "term":form["term"], "page":page, "searchtype":string.replace(form["searchtype"], " ", "%20")}
-                
-    myout.close()
+            if hit:
+                link = "<p><a href=../%(file)s target='text'>%(title)s</a><br>%(description)s</p>" % {"file":string.replace(hit.get("url"), " ", "%20"), "title":hit.get("title"), "description":hit.get("description")}
+                content = content + link            
+
+        if hits.length() > 20:
+            result = divmod(results, 20)
+            pages = result[0]
+            if not result[1] == 0:
+                pages = pages + 1
+            for page in range(1, pages + 1):
+                content = content + "[ <a href=/cgi-bin/%(scriptname)s?searchtype=%(searchtype)s&amp;term=%(term)s&amp;page=%(page)d>%(page)d</a> ]" % {"scriptname": scriptname, "term":form["term"].value, "page":page, "searchtype":string.replace(form["searchtype"].value, " ", "%20")}
+    if 0:
+        content = content + "Error running Lucene search. The error message returned by the server is: <br><br><code>"
+        import traceback
+        content = content + `traceback.print_exc()` + "</code>"
+        
 else:
     content = content + """
 <hr>
@@ -159,7 +103,7 @@ There are many techniques you can use to increase the results of your seach. Her
 <p>This search looks for pages with the word Titanic, but that do not have either the terms movie or film in them.</p>
     """
 
-templatefile = os.path.join(scriptdir, "default.tpl")
+templatefile = os.path.join("default.tpl")
 if os.path.exists(templatefile):
     myfile = open(templatefile, "rb")
     data = myfile.read()
@@ -167,8 +111,6 @@ if os.path.exists(templatefile):
     mypage = data
     mypage = string.replace(mypage, "--[name]--", "EClass Search Page")
     mypage = string.replace(mypage, "--[credit]--", "")
-    mypage = string.replace(mypage, "--[backlink]--", "")
-    mypage = string.replace(mypage, "--[nextlink]--", "")
     mypage = string.replace(mypage, "--[content]--", content)
     print mypage
 else:
