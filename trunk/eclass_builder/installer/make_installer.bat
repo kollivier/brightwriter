eb22837248b28c770c02a385b3bc732455117ac7
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2005-12-03 00:52:26 +0000

    Don't build a separate Unicode version for now.

--- trunk/eclass_builder/installer/make_installer.bat
+++ trunk/eclass_builder/installer/make_installer.bat
@@ -1,5 +1,5 @@
 C:\Python23\python ..\updateVersion.py
 make_py_dist.py
 C:\Progra~1\nsis\makensis eclass-builder.nsi
-C:\Python24\python make_py_dist.py --unicode
-C:\Progra~1\nsis\makensis eclass-builder.nsi
+REM C:\Python24\python make_py_dist.py --unicode
+REM C:\Progra~1\nsis\makensis eclass-builder.nsi
