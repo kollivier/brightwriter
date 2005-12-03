d7e9822985825e3096d520b14584fc317b96aac5
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2005-12-03 17:54:28 +0000

    Fixed to work both with Karrigell and with the cgi module.

--- eclass_builder/cgi-bin/search.py
+++ eclass_builder/cgi-bin/search.py
@@ -9,7 +9,9 @@ import win32api
 #from PyLucene import VERSION, LUCENE_VERSION
        
 scriptname = "search.py"
-scriptdir = os.path.dirname(os.path.abspath(sys.path[0]))
+scriptdir = os.path.abspath(sys.path[0])
+if not os.path.isdir(scriptdir):
+    scriptdir = os.path.dirname(scriptdir)
 eclassdir = "" #indicates current directory
 isiis = 0
 numresults_perpage = 20
@@ -23,8 +25,15 @@ numresults_perpage = 20
 #else:
 #    eclassdir = "../pub"
 
-form = cgi.FieldStorage()
-print "Content-type: text/html" # identify response as HTML
+form = {} 
+if QUERY:
+    form = QUERY
+    scriptdir = os.path.join(scriptdir, "cgi-bin")
+else:
+    myform = cgi.FieldStorage()
+    for item in myform:
+        form[item] = myform[item].value
+    print "Content-type: text/html" # identify response as HTML
 
 content = """
 """
@@ -35,28 +44,28 @@ if form.has_key("term"):
     field = "contents"
 
     if form.has_key("searchtype"):
-        if form["searchtype"].value == "Titles":
+        if form["searchtype"] == "Titles":
             field = "title"
-        elif form["searchtype"].value == "Keywords":
+        elif form["searchtype"] == "Keywords":
             field = "keywords"
 
     startitem = 1
     if form.has_key("page"):
-        startitem = 1 + (numresults_perpage * (int(form["page"].value) - 1))
+        startitem = 1 + (numresults_perpage * (int(form["page"]) - 1))
 
     if 1:
         directory = PyLucene.FSDirectory.getDirectory(os.path.join(scriptdir, "..", "index.lucene"), False)
         searcher = PyLucene.IndexSearcher(directory)
         analyzer = PyLucene.StandardAnalyzer()
-        query = PyLucene.QueryParser.parse(form["term"].value, field, analyzer)
+        query = PyLucene.QueryParser.parse(form["term"], field, analyzer)
         hits = searcher.search(query)
 
-        if hits.length() == 0:
+        if hits.length() <= 0:
             content = content + "Sorry, your search returned no results."
         else:
             content = content + "<p><i>Your search returned %(results)d results.</i></p>" % {"results":hits.length()}
 
-        for num in range(startitem, startitem + numresults_perpage):
+        for num in range(startitem-1, startitem-1 + numresults_perpage):
             hit = None
             if num < hits.length():
                 try:
@@ -67,13 +76,13 @@ if form.has_key("term"):
                 link = "<p><a href=../%(file)s target='text'>%(title)s</a><br>%(description)s</p>" % {"file":string.replace(hit.get("url"), " ", "%20"), "title":hit.get("title"), "description":hit.get("description")}
                 content = content + link            
 
-        if hits.length() > 20:
-            result = divmod(results, 20)
+        if hits.length() > numresults_perpage:
+            result = divmod(hits.length(), numresults_perpage)
             pages = result[0]
             if not result[1] == 0:
                 pages = pages + 1
             for page in range(1, pages + 1):
-                content = content + "[ <a href=/cgi-bin/%(scriptname)s?searchtype=%(searchtype)s&amp;term=%(term)s&amp;page=%(page)d>%(page)d</a> ]" % {"scriptname": scriptname, "term":form["term"].value, "page":page, "searchtype":string.replace(form["searchtype"].value, " ", "%20")}
+                content = content + "[ <a href=/cgi-bin/%(scriptname)s?searchtype=%(searchtype)s&amp;term=%(term)s&amp;page=%(page)d>%(page)d</a> ]" % {"scriptname": scriptname, "term":form["term"], "page":page, "searchtype":string.replace(form["searchtype"], " ", "%20")}
     if 0:
         content = content + "Error running Lucene search. The error message returned by the server is: <br><br><code>"
         import traceback
