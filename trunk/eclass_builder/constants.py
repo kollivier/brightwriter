d2c1d73481c7b322c1809f60962fc3c6edac164f
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2006-04-11 22:13:04 +0000

    Adding the constants module.

---
+++ trunk/eclass_builder/constants.py
@@ -0,0 +1,4 @@
+# constants.py - often used constant values
+
+errorInfoMsg = _(" Detailed error information can be found by selecting 'Tools->Error Viewer' from the menu.")
+createPageErrorMsg = _("There was an unknown error when creating the new page. The page was not created.")
