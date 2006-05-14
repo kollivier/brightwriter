3b4b9d6687ab1569f64593083ba70b439c98be15
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2006-05-14 03:47:59 +0000

    Added command to build the .exe to the installer script.

--- trunk/eclass_builder/installer/make_installer.bat
+++ trunk/eclass_builder/installer/make_installer.bat
@@ -2,6 +2,7 @@ call "%VS71COMNTOOLS%vsvars32.bat"
 REM Python24 gives an "invalid group reference" error
 C:\Python23\python ..\updateVersion.py
 C:\Python24\python make_py_dist.py
+nmake -f makefile.vc
 C:\Progra~1\nsis\makensis eclass-builder.nsi
 REM C:\Python24\python make_py_dist.py --unicode
 REM C:\Progra~1\nsis\makensis eclass-builder.nsi
