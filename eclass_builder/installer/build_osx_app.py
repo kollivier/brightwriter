1ab84c7488bb063033e2a3aba67b093aa68a7222
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2006-05-15 20:05:53 +0000

    Set the root dir so that it can be moved.

--- eclass_builder/installer/build_osx_app.py
+++ eclass_builder/installer/build_osx_app.py
@@ -1,33 +1,36 @@
 from distutils.core import setup
 import py2app
 import glob
+import version
 
 myplist = dict(
     CFBundleIdentifier='net.eclass.eclass_builder',
     CFBundleDisplayName="EClass.Builder",
-    CFBundleVersion="2.5.5.9"
+    CFBundleVersion=version.asString()
     )
-    
+ 
+rootdir = "./"
+
 py2app_options = dict(
-	iconfile="../icons/eclass_builder.icns", 
+	iconfile=rootdir + "icons/eclass_builder.icns", 
     argv_emulation=True,
     plist=myplist
 )
 
 setup(
-    app=['../eclass_builder.py'],
-    data_files=[('', 	glob.glob('../3rdparty') + 
-    						glob.glob('../about') +
-    						glob.glob('../autorun') +
-    						glob.glob('../convert') +
-    						glob.glob('../docs') +
-    						glob.glob('../greenstone') +
-    						glob.glob('../icons') +
-    						glob.glob('../locale') +
-    						glob.glob('../license') +
-    						glob.glob('../plugins') +
-    						['../swishe.conf'] +
-    						glob.glob('../themes')
+    name="EClass.Builder",
+    app=[rootdir + 'eclass_builder.py'],
+    data_files=[('', 	glob.glob(rootdir + '3rdparty') + 
+    						glob.glob(rootdir + 'about') +
+    						glob.glob(rootdir + 'autorun') +
+    						glob.glob(rootdir + 'convert') +
+    						glob.glob(rootdir + 'docs') +
+    						glob.glob(rootdir + 'greenstone') +
+    						glob.glob(rootdir + 'icons') +
+    						glob.glob(rootdir + 'locale') +
+    						glob.glob(rootdir + 'license') +
+    						glob.glob(rootdir + 'plugins') +
+    						glob.glob(rootdir + 'themes')
     						)
     			],
     options=dict(py2app=py2app_options),
