7eb895bd16358468ca1d78d11711adfefb5b8fef
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2006-06-01 19:20:31 +0000

    Change to our release numbering system to use dates for pre-releases rather than a special build number.

--- eclass_builder/version.py
+++ eclass_builder/version.py
@@ -3,7 +3,7 @@
 major = 2
 minor = 5
 release = 5
-build = 14
+#build = 14
 
 def asString():
-	return `major` + "." + `minor` + "." + `release` + "." + `build`
+	return `major` + "." + `minor` + "." + `release` #+ "." + `build`
