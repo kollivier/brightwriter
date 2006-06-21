0cf6800b462212fa9c8099f95d493588224dc36e
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2006-06-21 16:32:58 +0000

    Adding dialog to search through an EClass and present results.

---
+++ eclass_builder/gui/project_find_dialog.py
@@ -0,0 +1,65 @@
+import sys, string, os
+import utils
+import guiutils
+import time
+
+import wx
+import wxaddons.persistence
+import wxaddons.sized_controls as sc
+import errors
+import settings
+
+import index
+
+appErrorLog = errors.appErrorLog
+
+class ProjectFindDialog(sc.SizedDialog):
+    def __init__(self, parent=None):
+        sc.SizedDialog.__init__(self, parent, -1, _("Find In Project"), wx.DefaultPosition, wx.Size(420, 340), style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER)
+        pane = self.GetContentsPane()
+        self.itemCount = 0
+        
+        searchpane = sc.SizedPanel(pane, -1)
+        searchpane.SetSizerType("horizontal")
+        #searchpane.SetSizerProp("expand", "true")
+        #searchpane.SetSizerProp("proportion", 0)
+        
+        wx.StaticText(searchpane, -1, _("Find") + " ")
+        self.searchBox = wx.TextCtrl(searchpane, -1)
+        wx.StaticText(searchpane, -1, _("in") + " ")
+        self.searchContext = wx.Choice(searchpane, -1, choices=["contents"])
+        self.searchBtn = wx.Button(searchpane, -1, _("Search"))
+        
+        # list containing search results
+        self.listCtrl = wx.ListCtrl(pane, -1, style=wx.LC_REPORT)
+        self.listCtrl.SetSizerProp("expand", "true")
+        self.listCtrl.SetSizerProp("proportion", 2)
+        
+        self.itemCount = 0
+        self.selItem = None
+        self.listCtrl.InsertColumn(0, _("Page"))
+        self.listCtrl.InsertColumn(1, _("Filename"))
+        self.listCtrl.SetColumnWidth(0, 100)
+        self.listCtrl.SetColumnWidth(1, 280)
+
+        #EVT_LEFT_DCLICK(self, self.listCtrl.GetId(), self.OnDblClick)
+        wx.EVT_LIST_ITEM_SELECTED(self, self.listCtrl.GetId(), self.OnSelection)
+        wx.EVT_BUTTON(self.searchBtn, self.searchBtn.GetId(), self.OnSearch)
+
+        #wx.EVT_ACTIVATE(self, self.OnActivate)
+
+    def OnSearch(self, evt):
+        searcher = index.Index(self, os.path.join(settings.CurrentDir, "index.lucene"), settings.CurrentDir)
+        results = searcher.search("contents", self.searchBox.GetValue())
+        self.listCtrl.DeleteAllItems()
+        for result in results:
+            self.listCtrl.InsertStringItem(self.itemCount, result['title'])
+            self.listCtrl.SetStringItem(self.itemCount, 1, result['url'])
+            self.itemCount += 1
+            
+        
+            
+
+    def OnSelection(self, evt):
+        pass
+        
