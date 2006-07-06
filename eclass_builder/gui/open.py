131ee7a1d20e42014b61982be19fe1e3ea47cf39
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2006-07-06 22:29:34 +0000

    Make dialog HIG-compliant and make it stretchable.

--- eclass_builder/gui/open.py
+++ eclass_builder/gui/open.py
@@ -1,17 +1,52 @@
 import string, sys, os
-from wxPython.wx import *
+import wx
+import wxaddons.sized_controls as sc
+import wxaddons.persistence
 
-class OpenPubDialog(wxDialog):
+class OpenPubDialog(sc.SizedDialog):
 	def __init__(self, parent):
-		wxDialog.__init__ (self, parent, -1, _("Open Publication"), wxPoint(100,100),wxSize(480,200), wxDIALOG_MODAL|wxDEFAULT_DIALOG_STYLE)
-		height = 20
-		if wxPlatform == "__WXMAC__":
-			height = 25
+		sc.SizedDialog.__init__ (self, parent, -1, _("Open Publication"), (100,100), (480,200), wx.DIALOG_MODAL|wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER)
 		self.parent = parent
 		self.path = ""
-		self.lblSelect = wxStaticText(self, -1, _("Select a publication:"))
-		self.cmbpubs = wxListBox(self, -1, wxDefaultPosition, wxSize(-1, height*5))
+		
+		pane = self.GetContentsPane()
+		self.lblSelect = wx.StaticText(pane, -1, _("Select a publication:"))
+		self.cmbpubs = wx.ListBox(pane, -1)
+		self.cmbpubs.SetSizerProps({"expand": True, "proportion": 1})
 		self.coursedir = parent.settings["CourseFolder"]
+
+		btnPane = sc.SizedPanel(pane, -1)
+		btnPane.SetSizerType("horizontal")
+		btnPane.SetSizerProp("expand", True)
+		self.btnBrowse = wx.Button(btnPane, -1, _("Browse..."))
+		spacer = sc.SizedPanel(btnPane, -1)
+		spacer.SetSizerProps({"expand": True, "proportion": 1})
+		self.btnOK = wx.Button(btnPane,wx.ID_OK, _("OK"))
+		self.btnOK.SetDefault()
+		self.btnCancel = wx.Button(btnPane,wx.ID_CANCEL,_("Cancel"))
+
+		#self.buttonsizer.Add(self.btnBrowse, 0, wx.ALIGN_LEFT)
+		#self.buttonsizer.Add((1,1),1, wx.EXPAND | wx.ALL, 4)
+		stdbuttonsizer = wx.StdDialogButtonSizer()
+		stdbuttonsizer.AddButton(self.btnOK)
+		stdbuttonsizer.AddButton(self.btnCancel)
+		stdbuttonsizer.Realize()
+		btnPane.GetSizer().Add(stdbuttonsizer, 0, wx.ALIGN_CENTER)
+		#self.SetButtonSizer(self.buttonsizer)
+
+		self.LoadProjects()
+		self.cmbpubs.SetFocus()
+		if self.cmbpubs.GetCount() > 0:
+			self.cmbpubs.SetSelection(0)
+
+		self.Fit()
+		self.SetMinSize(self.GetSize())
+
+		wx.EVT_BUTTON(self.btnOK, self.btnOK.GetId(), self.btnOKClicked)
+		wx.EVT_BUTTON(self.btnBrowse, self.btnBrowse.GetId(), self.btnBrowseClicked)
+		wx.EVT_LEFT_DCLICK(self.cmbpubs, self.btnOKClicked)
+
+	def LoadProjects(self):
 		if os.path.exists(self.coursedir):
 			for item in os.listdir(self.coursedir):
 				try:
@@ -20,51 +55,21 @@ class OpenPubDialog(wxDialog):
 						self.cmbpubs.Append(item)
 				except:
 					pass
-		
-		self.btnBrowse = wxButton(self, -1, _("Browse..."))
-		self.btnOK = wxButton(self,wxID_OK, _("OK"))
-		self.btnOK.SetDefault()
-		self.cmbpubs.SetFocus()
-		if self.cmbpubs.GetCount() > 0:
-			self.cmbpubs.SetSelection(0)
-		self.btnCancel = wxButton(self,wxID_CANCEL,_("Cancel"))
-
-		self.mysizer = wxBoxSizer(wxVERTICAL)
-		self.mysizer.Add(self.lblSelect, 0, wxALL, 4)
-		self.mysizer.Add(self.cmbpubs, 1, wxEXPAND|wxALL, 4)
-		self.buttonsizer = wxBoxSizer(wxHORIZONTAL)
-		self.buttonsizer.Add(self.btnBrowse, 0, wxALIGN_LEFT)
-		self.buttonsizer.Add((1,1),1, wxEXPAND | wxALL, 4)
-		stdbuttonsizer = wxStdDialogButtonSizer()
-		stdbuttonsizer.AddButton(self.btnOK)
-		stdbuttonsizer.AddButton(self.btnCancel)
-		stdbuttonsizer.Realize()
-		self.buttonsizer.Add(stdbuttonsizer)
-		self.mysizer.Add(self.buttonsizer, 0, wxEXPAND | wxALL, 4)
-
-		self.SetAutoLayout(True)
-		self.SetSizer(self.mysizer)
-		self.mysizer.Fit(self)
-		self.Layout()
-
-		EVT_BUTTON(self.btnOK, self.btnOK.GetId(), self.btnOKClicked)
-		EVT_BUTTON(self.btnBrowse, self.btnBrowse.GetId(), self.btnBrowseClicked)
-		EVT_LEFT_DCLICK(self.cmbpubs, self.btnOKClicked)
 
 	def btnBrowseClicked(self, event):
 		dir = ""
-		f = wxDirDialog(self, _("Select the folder containing your EClass"))
-		if f.ShowModal() == wxID_OK:
+		f = wx.DirDialog(self, _("Select the folder containing your EClass"))
+		if f.ShowModal() == wx.ID_OK:
 			self.path = f.GetPath()
 			f.Destroy()
 			if not os.path.exists(os.path.join(self.path, "imsmanifest.xml")):
 				message = _("This folder does not contain an EClass Project. Please try selecting another folder.")
-				dialog = wxMessageDialog(self, message, "No EClass Found", wxOK)
+				dialog = wx.MessageDialog(self, message, "No EClass Found", wx.OK)
 				dialog.ShowModal()
 				dialog.Destroy()
 				return
 
-			self.EndModal(wxID_OK)
+			self.EndModal(wx.ID_OK)
 			
 	def GetPath(self):
 		return os.path.join(self.path, "imsmanifest.xml")
@@ -72,4 +77,4 @@ class OpenPubDialog(wxDialog):
 	def btnOKClicked(self, event):
 		if self.cmbpubs.GetStringSelection() != "":
 			self.path = os.path.join(self.coursedir, self.cmbpubs.GetStringSelection())
-			self.EndModal(wxID_OK)
+			self.EndModal(wx.ID_OK)
