23dead724e30e68aaffccad57e329f6bdb704bc8
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2006-07-25 00:23:02 +0000

    Removed long-broken OOo support code, and moved to wx namespace.

--- eclass_builder/eclass_builder.py
+++ eclass_builder/eclass_builder.py
@@ -1,7 +1,7 @@
 import string, sys, os
-from wxPython.wx import *
+import wx
 
-class MyApp(wxApp):
+class MyApp(wx.App):
 	def OnInit(self):
 		self.SetAppName("EClass.Builder")
 		import editor
@@ -11,35 +11,6 @@ class MyApp(wxApp):
 		return True
 
 for arg in sys.argv:
-	if arg == "--autostart-pyuno":
-		import re
-		myfilename = ""
-		if os.name == "nt":
-			myfilename = "C:\\Program Files\\OpenOffice.org1.1beta\\share\\registry\\data\\org\\openoffice\\Setup.xcu"
-		try:
-			print "Registering Pyuno... Location is: " + myfilename
-			file = utils.openFile(myfilename, "r")
-			data = file.read()
-			file.close()
-			file = utils.openFile(myfilename + ".bak", "w")
-			file.write(data)
-			file.close()
-			if string.find(data, "<prop oor:name=\"ooSetupConnectionURL\">") == -1:
-				if string.find(data, "<node oor:name=\"Office\">") > -1:
-					myterm = re.compile("(<node oor:name=\"Office\">)", re.IGNORECASE|re.DOTALL)
-					data = myterm.sub("\\1\n<prop oor:name=\"ooSetupConnectionURL\"><value>socket,host=localhost,port=2002;urp;</value></prop>\n", data)
-				else:
-					data = data + """
-					<node oor:name="Office">
-						<prop oor:name="ooSetupConnectionURL">socket,host=localhost,port=2002;urp;</prop>
-					</node>
-					"""
-				file = utils.openFile(myfilename, "w")
-				file.write(data)
-				file.close()
-		except:
-			print "Sorry, cannot register OpenOffice."
-		exit(0)
 	if arg == "--debug":
 		debug = 1
 
