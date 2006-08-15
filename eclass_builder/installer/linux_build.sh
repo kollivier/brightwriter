53e3e783e011fe059ec4a9ddc4f566afb0e9d0a0
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2006-08-15 00:08:18 +0000

    Make sure the encodings module is picked up.

--- eclass_builder/installer/linux_build.sh
+++ eclass_builder/installer/linux_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-~/cx_Freeze-3.0.2/FreezePython --install-dir deliver ../librarian.py
+~/cx_Freeze-3.0.2/FreezePython --incldue-modules = encodings --install-dir deliver ../librarian.py
 cp -r ../locale deliver
