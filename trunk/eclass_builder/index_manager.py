6e4426e9c81c4539e3b0d776ff7c52bacfffb1f0
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2007-02-07 19:41:03 +0000

    Add an export function.

--- trunk/eclass_builder/index_manager.py
+++ trunk/eclass_builder/index_manager.py
@@ -5,11 +5,16 @@ import index
 import utils
 import shutil
 import settings
+import tarfile
+import tempfile
 
 # constants
 
 INDEX_DIR = "index_directory"
 CONTENT_DIR = "content_directory"
+tempdir = ""
+tarName = ""
+extension = ".library"
 
 # Custom error classes
 class IndexExistsError(Exception):
@@ -23,13 +28,27 @@ class IndexNotFoundError(Exception):
         self.indexName = indexName
     def __str__(self):
         return "Index named '%s' not found." % self.indexName
+        
+def walker(archive, dirname, names):
+    global tempdir
+    global tarName
+    
+    for name in names:
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+            
+        fullpath = os.path.join(dirname, name)
+        if os.path.isfile(fullpath):
+            arcpath = fullpath.replace(tempdir, "")
+            print "Adding %s to archive" % (arcpath)            
+            archive.add(fullpath, arcpath)
 
 class IndexManager:
-    def __init__(self):
+    def __init__(self, cfgFile=""):
         self.indexes = ConfigParser.ConfigParser()
-        self.indexFile = "indexes.cfg"
+        self.indexFile = cfgFile
         self.indexes.read(self.indexFile)
-        self.indexesDir = os.path.join(settings.AppDir, "indexes")
+        self.indexesDir = os.path.join(os.path.dirname(cfgFile), "indexes")
         
         # Automatically load any projects in the indexes dir, if they have 
         # a contents directory.
@@ -91,6 +110,44 @@ class IndexManager:
         
     def saveChanges(self):
         self.indexes.write(open(self.indexFile, "w"))
+        
+    def exportIndex(self, name, dir="."):
+        if self.indexes.has_section(name):
+            print "Exporting index properties for %s" % name
+
+            global tempdir
+            global tarName
+            global extension
+            
+            tempdir = tempfile.mkdtemp()
+            exportConfig = ConfigParser.ConfigParser()
+            exportConfig.add_section(name)
+            for opt in self.indexes.options(name):
+                exportConfig.set(name, opt, self.getIndexProp(name, opt))
+                
+            exportConfig.set(name, INDEX_DIR, "indexes")
+            exportConfig.set(name, CONTENT_DIR, "contents")
+                
+            exportConfig.write(open(os.path.join(tempdir, "indexes.cfg"), "w"))
+            
+            print "Copying index files (this may take a while)..."
+            import shutil
+            shutil.copytree(self.getIndexProp(name, INDEX_DIR), os.path.join(tempdir, INDEX_DIR))
+            
+            print "Copying content files (this may take a while)..."
+            shutil.copytree(self.getIndexProp(name, INDEX_DIR), os.path.join(tempdir, CONTENT_DIR))
+            
+            tarName = utils.createSafeFilename(name)
+            archive = tarfile.open(os.path.join(dir, tarName + extension), "w:bz2")
+            os.path.walk(tempdir, walker, archive)
+            archive.close()
+            
+            shutil.rmtree(tempdir)
+            
+    def importIndex(self, name, archive):
+        name = os.path.splitext(os.path.basename(archive))[0]
+        print "importing library %s... " % name
+        
         
     def removeIndex(self, name, deleteIndexFiles=True):
         if self.indexes.has_section(name):
