8c97306481d09dd79d7da7eb6d61c11aa8dd1507
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2007-03-28 21:12:45 +0000

    Create a separate Mac Intel build script.

---
+++ eclass_builder/installer/build-mac-intel.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#----------------------------------------------------------------------
+
+set -o errexit
+
+if [ "$skipmac" != "yes" ]; then
+    MAC_HOST=$MAC_HOST_INTEL
+    BUILD_TYPE="universal"
+
+    echo "Copying source file and build script..."
+    cp -r $STAGING_DIR/* $MAC_BUILD
+    
+     echo "Untarring dist on $MAC_HOST..."
+     tarball=$MAC_BUILD/eclass.builder-$BUILD_VERSION.tar.gz
+     cmd="tar xzvf"
+     $cmd $tarball
+     
+     echo "Running build script on $MAC_HOST..."
+     dir=$MAC_BUILD/eclass_builder
+     cmd="export PYTHONPATH=.. && /usr/local/bin/python2.4 make_builder_osx.py py2app && /usr/local/bin/python2.4 make_library_osx.py py2app"
+     cd $dir/installer && rm -rf build dist && $cmd
+
+     # now it's time for 'fun fixing the PyXML hacks!'
+     #OLDDIR=$PWD
+     APP_PYDIR=$dir/installer/dist/EClass.Builder.app/Contents/Resources/lib/python2.4
+     cd $APP_PYDIR && mv lib-dynload/xml/parsers/pyexpat.so lib-dynload && mv lib-dynload/xml/parsers/sgmlop.so lib-dynload
+     zip $APP_PYDIR/site-packages.zip -d xml/*
+     cd /Library/Frameworks/Python.framework/Versions/2.4/lib/python2.4 && zip -r -g -n .pyc $APP_PYDIR/site-packages.zip . -i xml\*.pyc && cd site-packages && zip -r -g -n .pyc:.mo $APP_PYDIR/site-packages . -i _xmlplus\*.pyc _xmlplus\*.mo
+
+     echo "Fetching the results..."
+     mkdir -p $DIST_DIR/dmg_files
+     cp -r $dir/installer/dist/EClass.Builder.app $DIST_DIR/dmg_files
+     #cd $OLDDIR
+
+     DMG_NAME=deliver/eclass-builder-$BUILD_VERSION-$BUILD_TYPE.dmg
+     if [ -f $DMG_NAME ]; then
+       rm $DMG_NAME
+     fi
+     
+     hdiutil create -srcfolder $DIST_DIR/dmg_files -volname "EClass.Builder" -imagekey zlib-level=9 $DMG_NAME
+     
+     rm -rf $DIST_DIR/dmg_files
+
+     scp -r "$MAC_HOST:$dir/installer/dist/EClass.Library.app "  $DIST_DIR/dmg_files
+     DMG_NAME=deliver/eclass-library-$LIBRARY_VERSION-$BUILD_TYPE.dmg
+     if [ -f $DMG_NAME ]; then
+       rm $DMG_NAME
+     fi
+     
+     hdiutil create -srcfolder $DIST_DIR/dmg_files -volname "EClass.Library" -imagekey zlib-level=9 $DMG_NAME
+     
+     rm -rf $DIST_DIR/dmg_files
+     #hdiutil attach $DMG_NAME
+     
+     #cp -r deliver/EClass.Builder.app /Volumes/EClass.Builder
+     
+     #hdiutil detach /Volumes/EClass.Builder
+     
+     echo "Done!"
+fi
