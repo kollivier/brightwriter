fafdb62794462d52f3c53ddcc76e1f13923ba44e
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2007-08-07 00:29:17 +0000

    Allow this file to be loaded even when not being run from the GUI.

--- eclass_builder/constants.py
+++ eclass_builder/constants.py
@@ -2,6 +2,10 @@
 
 eclassdirs = ["EClass", "Text", "pub", "Graphics", "File", "Present"]
 
+if not "_" in dir():
+    def _(text):
+        return text
+
 errorInfoMsg = _(" Detailed error information can be found by selecting 'Tools->Error Viewer' from the menu.")
 createPageErrorMsg = _("There was an unknown error when creating the new page. The page was not created.")
 
