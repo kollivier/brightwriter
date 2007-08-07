56e50acc4a364a13eb2b9e228992fefb1f09138e
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2007-08-07 00:26:51 +0000

    Move to wx namespace.

--- trunk/eclass_builder/gui/about.py
+++ trunk/eclass_builder/gui/about.py
@@ -1,20 +1,20 @@
 import string, sys, os
-from wxPython.wx import *
+import wx
 import wxbrowser
 import settings
 
-class EClassAboutDialog(wxDialog):
+class EClassAboutDialog(wx.Dialog):
 	def __init__(self, parent):
-		wxDialog.__init__ (self, parent, -1, _("About EClass.Builder"), wxPoint(100,100),wxSize(460,400), wxDIALOG_MODAL|wxDEFAULT_DIALOG_STYLE)
+		wx.Dialog.__init__ (self, parent, -1, _("About EClass.Builder"), wx.Point(100,100),wx.Size(460,400), wx.DIALOG_MODAL|wx.DEFAULT_DIALOG_STYLE)
 		self.parent = parent
 		self.browser = wxbrowser.wxBrowser(self, -1)
 		self.browser.LoadPage(os.path.join(settings.AppDir,"about", parent.langdir, "about_eclass.html"))
 		
-		self.btnOK = wxButton(self,wxID_OK,_("OK"))
+		self.btnOK = wx.Button(self,wx.ID_OK,_("OK"))
 		self.btnOK.SetDefault()
-		self.mysizer = wxBoxSizer(wxVERTICAL)
-		self.mysizer.Add(self.browser.browser, 1, wxEXPAND|wxALL, 4)
-		self.mysizer.Add(self.btnOK, 0, wxALIGN_CENTER|wxALL, 6)			
+		self.mysizer = wx.BoxSizer(wx.VERTICAL)
+		self.mysizer.Add(self.browser.browser, 1, wx.EXPAND|wx.ALL, 4)
+		self.mysizer.Add(self.btnOK, 0, wx.ALIGN_CENTER|wx.ALL, 6)			
 
 		self.SetAutoLayout(True)
 		self.SetSizer(self.mysizer)
@@ -23,4 +23,4 @@ class EClassAboutDialog(wxDialog):
 		EVT_BUTTON(self.btnOK, self.btnOK.GetId(), self.btnOKClicked)	
 
 	def btnOKClicked(self, event):
-		self.EndModal(wxID_OK)
+		self.EndModal(wx.ID_OK)
