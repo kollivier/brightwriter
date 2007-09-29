07c549cd08b3cb28d862c793a3c07ae91ceaba13
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2007-09-29 19:16:59 +0000

    Restore preview and link check functions, and also detab utils/__init__.py.

--- eclass_builder/utils/__init__.py
+++ eclass_builder/utils/__init__.py
@@ -8,48 +8,54 @@ import sys, os, string
 import settings
 import plugins
 import constants
+import ims
+import ims.contentpackage
+import ims.utils
+import conman
+import appdata
+import eclassutils
 
 filenameRestrictedChars = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|"]
 
 class LogFile:
-	def __init__(self, filename="log.txt"):
-		self.filename = filename
-		
-	def read(self):
-		if os.path.exists(self.filename):
-			return unicode(open(self.filename, "rb").read(), "utf-8")
-		else:
-			return ""
+    def __init__(self, filename="log.txt"):
+        self.filename = filename
+        
+    def read(self):
+        if os.path.exists(self.filename):
+            return unicode(open(self.filename, "rb").read(), "utf-8")
+        else:
+            return ""
 
-	def write(self, message):
-		if message == None:
-			return
-		myfile = open(self.filename, "ab")
-		myfile.write(message.encode("utf-8") + "\n")
-		myfile.close()
+    def write(self, message):
+        if message == None:
+            return
+        myfile = open(self.filename, "ab")
+        myfile.write(message.encode("utf-8") + "\n")
+        myfile.close()
 
-	def clear(self):
-		os.remove(self.filename)
+    def clear(self):
+        os.remove(self.filename)
 
 def getStdErrorMessage(type = "IOError", args={}):
-	if type == "IOError":
-		if args.has_key("type") and args["type"] == "write":
-			return _("There was an error writing the file '%(filename)s' to disk. Please check that you have enough hard disk space to write this file and that you have permission to write to the file.") % {"filename":args["filename"]}
-		elif args.has_key("type") and args["type"] == "read":
-			return _("Could not read file '%(filename)s from disk. Please check that the file exists in the location specified and that you have permission to open/view the file.") % {"filename":args["filename"]}
-		elif args.has_key("filename"):
-			return _("There was a problem reading or writing the file %(filename)s. Please check that the file exists and that you have correct permissions to the file.") % {"filename":args["filename"]} 
-	elif type == "UnknownError":
-		return _("An unknown error has occurred.") + constants.errorInfoMsg
+    if type == "IOError":
+        if args.has_key("type") and args["type"] == "write":
+            return _("There was an error writing the file '%(filename)s' to disk. Please check that you have enough hard disk space to write this file and that you have permission to write to the file.") % {"filename":args["filename"]}
+        elif args.has_key("type") and args["type"] == "read":
+            return _("Could not read file '%(filename)s from disk. Please check that the file exists in the location specified and that you have permission to open/view the file.") % {"filename":args["filename"]}
+        elif args.has_key("filename"):
+            return _("There was a problem reading or writing the file %(filename)s. Please check that the file exists and that you have correct permissions to the file.") % {"filename":args["filename"]} 
+    elif type == "UnknownError":
+        return _("An unknown error has occurred.") + constants.errorInfoMsg
 
 def makeModuleName(text):
-	result = string.replace(text, "-", "_")
-	result = string.replace(result, "*", "")
-	result = string.replace(result, "/", "")
-	result = string.replace(result, "\\", "")
+    result = string.replace(text, "-", "_")
+    result = string.replace(result, "*", "")
+    result = string.replace(result, "/", "")
+    result = string.replace(result, "\\", "")
 
 def createHTMLPageWithBody(text):
-	retval = """
+    retval = """
 <html>
 <head>
 </head>
@@ -58,7 +64,7 @@ def createHTMLPageWithBody(text):
 </body>
 </html>""" % text
 
-	return retval
+    return retval
 
 def isReadOnly(filename):
     if sys.platform == 'win32':
@@ -69,76 +75,90 @@ def isReadOnly(filename):
         return not (os.stat(filename)[stat.ST_MODE] & stat.S_IWUSR)
 
 def CreateJoustJavascript(pub):
-	try:
-		filename = GetFileLink(pub.nodes[0].content.filename)
-		text = u"""
+    if isinstance(pub, conman.conman.ConNode):
+        name = pub.nodes[0].content.metadata.name
+        filename = GetFileLink(pub.content.filename)
+    elif isinstance(pub, ims.contentpackage.Item):
+        name = pub.title.text
+        resource = ims.utils.getIMSResourceForIMSItem(appdata.activeFrame.imscp, pub)
+        filename = eclassutils.getEClassPageForIMSResource(resource)
+        if not filename:
+            filename = resource.attrs["href"]
+    text = u"""
 function addJoustItems(theMenu){
-	var level1ID = -1;
-	var level2ID = -1;
-	var level3ID = -1;
+var level1ID = -1;
+var level2ID = -1;
+var level3ID = -1;
 """
-		text = text + """level1ID = theMenu.addEntry(-1, "Book", "%s", "%s", "%s");\n""" % (string.replace(pub.nodes[0].content.metadata.name, "\"", "\\\""), filename, string.replace(pub.nodes[0].content.metadata.name, "\"", "\\\""))
-		text = text + AddJoustItems(pub.nodes[0], 1)
-		text = text + "return theMenu; \n}"
+    text = text + """level1ID = theMenu.addEntry(-1, "Book", "%s", "%s", "%s");\n""" % (string.replace(name, "\"", "\\\""), filename, string.replace(name, "\"", "\\\""))
+    text = text + AddJoustItems(pub, 1)
+    text = text + "return theMenu; \n}"
 
-		afile = open(os.path.join(settings.ProjectDir, "joustitems.js"), "w")
-		afile.write(text.encode("utf-8"))
-		afile.close()
-	except:
-		import traceback
-		print `traceback.print_exc()`
-	
+    afile = open(os.path.join(settings.ProjectDir, "joustitems.js"), "w")
+    afile.write(text.encode("utf-8"))
+    afile.close()
+    
 
 def AddJoustItems(nodes, level):
-	text = u""
-	for root in nodes.children:
-		filename = ""
-		if string.find(root.content.filename, "imsmanifest.xml") != -1:
-				root = root.pub.nodes[0]
+    text = u""
+    if isinstance(nodes, conman.conman.ConNode):
+        children = nodes.children
+    else:
+        children = nodes.items
+        
+    for root in children:
+        filename = ""
 
-		filename = GetFileLink(root.content.filename) 
+        if isinstance(root, conman.conman.ConNode):
+            filename = GetFileLink(root.content.filename)
+            name = root.content.metadata.name
+        elif isinstance(root, ims.contentpackage.Item):
+            name = root.title.text
+            resource = ims.utils.getIMSResourceForIMSItem(appdata.activeFrame.imscp, root)
+            filename = eclassutils.getEClassPageForIMSResource(resource)
+            if not filename:
+                filename = resource.attrs["href"]
 
-		if not root.content.public == "false":
-			if len(root.children) > 0:
-				nodeType = "Book"
-			else:
-				nodeType = "Document"
-			text = text + u"""level%sID = theMenu.addChild(level%sID,"%s", "%s", "%s", "%s");\n""" % (level + 1, level, nodeType, string.replace(root.content.metadata.name, '"', '\\"'), filename, string.replace(root.content.metadata.name, '"', '\\"'))
+        if isinstance(root, conman.conman.ConNode) and len(root.children) > 0 \
+            or isinstance(root, ims.contentpackage.Item) and len(root.items) > 0:
+            nodeType = "Book"
+        else:
+            nodeType = "Document"
+        text = text + u"""level%sID = theMenu.addChild(level%sID,"%s", "%s", "%s", "%s");\n""" % (level + 1, level, nodeType, string.replace(name, '"', '\\"'), filename, string.replace(name, '"', '\\"'))
 
-			if len(root.children) > 0:
-				text = text + AddJoustItems(root, level + 1)
-		else:
-			print "Item " + root.content.metadata.name + " is marked private and was not published."
-	return text	
+        if len(root.children) > 0:
+            text = text + AddJoustItems(root, level + 1)
+
+    return text 
 
 def GetFileLink(filename):
-	try:
-		publisher = plugins.GetPluginForFilename(filename).HTMLPublisher()
-		filename = publisher.GetFileLink(filename)
-	except: 
-		filename = string.replace(filename, "\\", "/")
-		
-	return filename
+    try:
+        publisher = plugins.GetPluginForFilename(filename).HTMLPublisher()
+        filename = publisher.GetFileLink(filename)
+    except: 
+        filename = string.replace(filename, "\\", "/")
+        
+    return filename
 
 def getCurrentEncoding():
-	import locale
-	encoding = locale.getdefaultlocale()[1]
-	if not encoding or encoding == 'ascii':
-		if sys.platform == "darwin":
-			encoding = "utf-8"
-		else:
-			encoding = "iso-8859-1" 
-	
-	return encoding
-	
+    import locale
+    encoding = locale.getdefaultlocale()[1]
+    if not encoding or encoding == 'ascii':
+        if sys.platform == "darwin":
+            encoding = "utf-8"
+        else:
+            encoding = "iso-8859-1" 
+    
+    return encoding
+    
 def makeUnicode(text, encoding=""):
-	if encoding == "":
-		encoding = getCurrentEncoding()
+    if encoding == "":
+        encoding = getCurrentEncoding()
 
-	if isinstance(text, str):
-		return text.decode(encoding, 'replace')
-	else:
-		return text
+    if isinstance(text, str):
+        return text.decode(encoding, 'replace')
+    else:
+        return text
 
 def openFile(filename, mode="r"):
     """We need this special function because there are hardcoded restriction on the size of the pathname. 
@@ -167,13 +187,13 @@ def createSafeFilename(filename):
     for char in filenameRestrictedChars:
         finalname = finalname.replace(char, "")
 
-	return finalname
-	
+    return finalname
+    
 def createWebFriendlyFilename(filename):
     finalname = createSafeFilename(filename)
     finalname = finalname.replace(" ", "_")
     return finalname
-	
+    
 def checkNameExists(filename):
     # due to file conversion, we have to check for files with other extensions
     # that have the same name.
@@ -194,8 +214,8 @@ def suggestFilename(filename):
         basename, ext = os.path.splitext(os.path.basename(finalname))
         finalname = basename + `counter` + ext
         counter = counter + 1
-	return finalname
-	
+    return finalname
+    
 def escapeFilename(filename):
     # escape any special characters for terminal commands
     if not sys.platform.startswith("win"):
