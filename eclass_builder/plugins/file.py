e0b4883ffa22c46d28dfe7026e36096fbaad0ccf
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2007-10-17 19:11:27 +0000

    Detab file.py.

--- eclass_builder/plugins/file.py
+++ eclass_builder/plugins/file.py
@@ -1,36 +1,36 @@
 import string, sys, os
 import plugins
 
-plugin_info = {	"Name":"file", 
-				"FullName":"File", 
-				"Directory":"File", 
-				"Extension":["*"], 
-				"Mime Type": "",
-				"IMS Type": "webcontent",
-				"Requires":"",
-				"CanCreateNew":False}
+plugin_info = { "Name":"file", 
+                "FullName":"File", 
+                "Directory":"File", 
+                "Extension":["*"], 
+                "Mime Type": "",
+                "IMS Type": "webcontent",
+                "Requires":"",
+                "CanCreateNew":False}
 
 
 class HTMLPublisher(plugins.BaseHTMLPublisher):
 
-	def GetData(self):
-		return None
+    def GetData(self):
+        return None
 
 
 class EditorDialog:
-	def __init__(self, parent, item):
-		self.item = item
-		self.parent = parent
+    def __init__(self, parent, item):
+        self.item = item
+        self.parent = parent
 
-	def ShowModal(self):
-		import guiutils
-		myFilename = os.path.join(settings.ProjectDir, self.item.content.filename)
-		result = False
+    def ShowModal(self):
+        import guiutils
+        myFilename = os.path.join(settings.ProjectDir, self.item.content.filename)
+        result = False
 
-		if os.path.exists(myFilename):
-			result = guiutils.sendCommandToApplication(myFilename, "open")
+        if os.path.exists(myFilename):
+            result = guiutils.sendCommandToApplication(myFilename, "open")
 
-		if not result:
-			result = PagePropertiesDialog(parent, parent.CurrentItem, parent.CurrentItem.content, os.path.join(parent.ProjectDir, "Text")).ShowModal()
+        if not result:
+            result = PagePropertiesDialog(parent, parent.CurrentItem, parent.CurrentItem.content, os.path.join(parent.ProjectDir, "Text")).ShowModal()
 
-		return result
+        return result
