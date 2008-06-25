23fc66389a9c15541194c61c74d8ca4b26c64be4
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2008-06-25 00:22:06 +0000

    Use Py 2.5 for build.

--- eclass_builder/installer/make_installer.bat
+++ eclass_builder/installer/make_installer.bat
@@ -1,13 +1,13 @@
 call "%VS71COMNTOOLS%vsvars32.bat"
 REM Python24 gives an "invalid group reference" error
-C:\Python24\python ..\updateVersion.py %1
-C:\Python24\python make_py_dist.py --unicode
+C:\Python25\python ..\updateVersion.py %1
+C:\Python25\python make_py_dist.py --unicode
 C:\cx_Freeze-3.0.3\FreezePython --exclude-modules=wx --install-dir librarian-win32 ../librarian.py
 nmake -f makefile.vc
 cd ..\autorun\loader
 nmake -f loader.mak
 cd ..\..
-C:\Python24\python runTests.py
+C:\Python25\python runTests.py
 cd installer
 C:\Progra~1\nsis\makensis eclass-builder.nsi
 REM C:\Python24\python make_py_dist.py --unicode
