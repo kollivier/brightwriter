ef0456031d88a961dc6762dd20404a69a8fdaa66
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2008-08-06 21:57:06 +0000

    Try and get it to pick up wx.lib.sized_controls.

--- trunk/eclass_builder/installer/make_py_dist.py
+++ trunk/eclass_builder/installer/make_py_dist.py
@@ -58,7 +58,7 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), "..
 modfinder = modulefinder.ModuleFinder(excludes=["Tkinter"])
 modulefinder.ReplacePackage("xml", "_xmlplus")
 modfinder.add_module("site")
-modfinder.add_module("wxaddons.wxblox")
+modfinder.add_module("wx.lib.sized_controls")
 
 scripts = [os.path.join("..", "eclass_builder.py"), os.path.join("..", "editor.py"), os.path.join("..", "converter.py")]
 deps = []
