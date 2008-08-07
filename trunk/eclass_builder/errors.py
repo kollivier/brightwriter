ce63251cb6857c8af54b7386a17fd5b45a0fe24d
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2008-08-07 21:39:19 +0000

    Provide more information about the current error.

--- trunk/eclass_builder/errors.py
+++ trunk/eclass_builder/errors.py
@@ -9,10 +9,46 @@ def getTraceback():
 	type, value, trace = sys.exc_info()
 	list = traceback.format_exception_only(type, value) + ["\n"] + traceback.format_tb(trace)
 	return string.join(list, "")
+
+def exceptionAsString(exctype, value):
+	return string.join(traceback.format_exception(exctype, value, None), "\n")	
 	
-def exceptionAsString(exctype, value, trace):
-	return string.join(traceback.format_exception(exctype, value, trace), "\n")
-	
+# taken from http://code.activestate.com/recipes/52215/
+def print_exc_plus(exctype, value, trace):
+    """
+    Print the usual traceback information, followed by a listing of all the
+    local variables in each frame.
+    """
+    tb = trace
+    stack = []
+    
+    while tb:
+        stack.append(tb.tb_frame)
+        tb = tb.tb_next
+
+    #exception = exceptionAsString(exctype, value, trace)
+    exception = "Traceback (most recent call last):\n"
+    for frame in stack:
+        exception += "\n"
+        exception += "File \"%s\", line %s, in %s\n" % (frame.f_code.co_filename,
+                                             frame.f_lineno,
+                                             frame.f_code.co_name)
+        for key, keyvalue in frame.f_locals.items():
+            exception += "        %s = " % `key`
+            #We have to be careful not to cause a new error in our error
+            #printer! Calling str() on an unknown object could cause an
+            #error we don't want.
+            try:                   
+                exception += `keyvalue`
+            except:
+                exception += "<ERROR WHILE PRINTING VALUE>"
+            exception += "\n"
+        exception += "\n"
+            
+    exception += exceptionAsString(exctype, value)        
+    
+    return exception
+
 def exceptionHook(exctype, value, trace):
     print exceptionAsString(exctype, value, trace)
 	
