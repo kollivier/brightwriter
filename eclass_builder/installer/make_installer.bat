ffe92ae462affb6f480028a4fd3e8edbf597e380
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2008-11-15 17:50:51 +0000

    Update to Python 2.6

--- eclass_builder/installer/make_installer.bat
+++ eclass_builder/installer/make_installer.bat
@@ -1,14 +1,11 @@
 call "%VS71COMNTOOLS%vsvars32.bat"
-REM Python24 gives an "invalid group reference" error
-C:\Python25\python ..\updateVersion.py %1
-C:\Python25\python make_py_dist.py --unicode
-C:\cx_Freeze-3.0.3\FreezePython --exclude-modules=wx --install-dir librarian-win32 ../librarian.py
+C:\Python26\python ..\updateVersion.py %1
+C:\Python26\python make_py_dist.py --unicode
+C:\Python26\Scripts\cxfreeze --exclude-modules=wx --install-dir librarian-win32 ../librarian.py
 nmake -f makefile.vc
 cd ..\autorun\loader
 nmake -f loader.mak
 cd ..\..
-C:\Python25\python runTests.py
+C:\Python26\python runTests.py
 cd installer
 C:\Progra~1\nsis\makensis eclass-builder.nsi
-REM C:\Python25\python make_py_dist.py --unicode
-REM C:\Progra~1\nsis\makensis eclass-builder.nsi
