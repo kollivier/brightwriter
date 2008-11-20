e4d559758416b73936d92c4c0d08135defde0c31
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2008-11-20 22:37:24 +0000

    Use new version of cx_freeze.

--- trunk/eclass_builder/installer/make_installer.bat
+++ trunk/eclass_builder/installer/make_installer.bat
@@ -1,7 +1,6 @@
 call "%VS90COMNTOOLS%vsvars32.bat"
 C:\Python26\python ..\updateVersion.py %1
 C:\Python26\python make_py_dist.py --unicode
-C:\Python26\Scripts\cxfreeze --exclude-modules=wx --install-dir librarian-win32 ../librarian.py
 nmake -f makefile.vc
 cd ..\autorun\loader
 nmake -f loader.mak
