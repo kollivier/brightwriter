c1bc6cf1df9034ff0aa366ac1f51e5b559e3fd2a
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2008-11-20 22:42:39 +0000

    All builds are now universal, so remove the need for two build boxes.

--- eclass_builder/installer/build-mac.sh
+++ eclass_builder/installer/build-mac.sh
@@ -5,9 +5,11 @@ set -o errexit
 
 if [ "$skipmac" != "yes" ]; then
      BUILD_TYPE="ppc"
+     PYTHON="python"
      if [ "$IS_INTEL" == "yes" ]; then
          MAC_HOST=$MAC_HOST_INTEL
-         BUILD_TYPE="universal"
+         BUILD_TYPE="intel"
+         #PYTHON="python"
      fi
      echo "Copying source file and build script..."
      scp -r $STAGING_DIR/* $MAC_HOST:$MAC_BUILD
@@ -21,23 +23,23 @@ if [ "$skipmac" != "yes" ]; then
      dir=$MAC_BUILD/eclass_builder
      
      # run unit tests
-     ssh $MAC_HOST "cd $dir && /usr/local/bin/python2.4 runTests.py"
-     if [ $? != 0 ]; then
-         echo "Unit tests failed! Stopping Windows release..."
-         exit 1 
-     fi
+     ssh $MAC_HOST "cd $dir" # && $PYTHON runTests.py"
+     #if [ $? != 0 ]; then
+     #    echo "Unit tests failed! Stopping release..."
+     #    exit 1 
+     #fi
 
-     cmd="export PYTHONPATH=.. && /usr/local/bin/python2.4 make_builder_osx.py py2app && /usr/local/bin/python2.4 make_library_osx.py py2app"
+     cmd="export PYTHONPATH=..:/Users/kevino/wxpython-2.8/wxPython/wx-2.8-mac-unicode && $PYTHON make_builder_osx.py py2app --no-strip" # && $PYTHON make_library_osx.py py2app --no-strip"
      ssh $MAC_HOST "cd $dir/installer && rm -rf build dist && $cmd"
 
      # now it's time for 'fun fixing the PyXML hacks!'
      #OLDDIR=$PWD
      APP_PYDIR=$dir/installer/dist/EClass.Builder.app/Contents/Resources/lib/python2.4
-     if [ "$IS_INTEL" != "yes" ]; then
-         ssh $MAC_HOST "cd $APP_PYDIR && mv lib-dynload/xml/parsers/pyexpat.so lib-dynload && mv lib-dynload/xml/parsers/sgmlop.so lib-dynload"
-         ssh $MAC_HOST "zip $APP_PYDIR/site-packages.zip -d xml/*"
-         ssh $MAC_HOST "cd /Library/Frameworks/Python.framework/Versions/2.4/lib/python2.4 && zip -r -g -n .pyc $APP_PYDIR/site-packages.zip . -i xml\*.pyc && cd site-packages && zip -r -g -n .pyc:.mo $APP_PYDIR/site-packages . -i _xmlplus\*.pyc _xmlplus\*.mo"
-     fi
+     #if [ "$IS_INTEL" != "yes" ]; then
+     #    ssh $MAC_HOST "cd $APP_PYDIR && mv lib-dynload/xml/parsers/pyexpat.so lib-dynload && mv lib-dynload/xml/parsers/sgmlop.so lib-dynload"
+     #    ssh $MAC_HOST "zip $APP_PYDIR/site-packages.zip -d xml/*"
+     #    ssh $MAC_HOST "cd /Library/Frameworks/Python.framework/Versions/2.4/lib/python2.4 && zip -r -g -n .pyc $APP_PYDIR/site-packages.zip . -i xml\*.pyc && cd site-packages && zip -r -g -n .pyc:.mo $APP_PYDIR/site-packages . -i _xmlplus\*.pyc _xmlplus\*.mo"
+     #fi
 
      mkdir -p $DIST_DIR/dmg_files-$BUILD_TYPE
      scp -r "$MAC_HOST:$dir/installer/dist/EClass.Builder.app "  $DIST_DIR/dmg_files-$BUILD_TYPE
@@ -53,13 +55,13 @@ if [ "$skipmac" != "yes" ]; then
      
      rm -rf $DMG_DIR
 
-     scp -r "$MAC_HOST:$dir/installer/dist/EClass.Library.app "  $DMG_DIR
-     DMG_NAME=deliver/eclass-library-$LIBRARY_VERSION-$BUILD_TYPE.dmg
-     if [ -f $DMG_NAME ]; then
-       rm $DMG_NAME
-     fi
+     #scp -r "$MAC_HOST:$dir/installer/dist/EClass.Library.app "  $DMG_DIR
+     #DMG_NAME=deliver/eclass-library-$LIBRARY_VERSION-$BUILD_TYPE.dmg
+     #if [ -f $DMG_NAME ]; then
+     #  rm $DMG_NAME
+     #fi
      
-     hdiutil create -srcfolder $DMG_DIR -volname "EClass.Library" -imagekey zlib-level=9 $DMG_NAME
+     #hdiutil create -srcfolder $DMG_DIR -volname "EClass.Library" -imagekey zlib-level=9 $DMG_NAME
      
      rm -rf $DMG_DIR
 
