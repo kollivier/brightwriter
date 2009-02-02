e9a1177d291d37d5491d341df5205323699fc298
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2009-02-02 19:28:56 +0000

    Silence a couple warnings.

--- eclass_builder/plugins/quiz.py
+++ eclass_builder/plugins/quiz.py
@@ -271,6 +271,8 @@ class QuizPage(plugins.PluginData):
         Return values:
         Returns an error string if failed, or an empty string if successful.
         """
+        global log
+            
         if filename == "":
             filename = self.filename
         else:
@@ -280,7 +282,6 @@ class QuizPage(plugins.PluginData):
             myxml = """<?xml version="1.0"?>%s""" % (self.WriteDoc())
         except:
             message = _("There was an error updating the file %(filename)s. Please check to make sure you did not enter any invalid characters (i.e. Russian, Chinese/Japanese, Arabic) and try updating again.") % {"filename":filename}
-            global log
             log.write(message)
             raise IOError, message
         try:
@@ -296,7 +297,6 @@ class QuizPage(plugins.PluginData):
             myfile.write(myxml)
             myfile.close()
         except:
-            global log
             message = utils.getStdErrorMessage("IOError", {"type":"write", "filename": filename})
             log.write(message)
             raise IOError(message)
