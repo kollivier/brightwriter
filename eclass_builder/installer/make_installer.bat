41417f922650df45760f6a88e3994cdf2eb0c5f6
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2009-02-02 21:12:54 +0000

    Move to using make-installer.py on both Windows and Mac.

--- eclass_builder/installer/make_installer.bat
+++ eclass_builder/installer/make_installer.bat
@@ -1,10 +1,10 @@
 call "%VS90COMNTOOLS%vsvars32.bat"
-C:\Python26\python ..\updateVersion.py %1
-C:\Python26\python make_py_dist.py --unicode
+C:\Python25\python ..\updateVersion.py %1
 nmake -f makefile.vc
 cd ..\autorun\loader
 nmake -f loader.mak
 cd ..\..
-C:\Python26\python runTests.py
+C:\Python25\python runTests.py
 cd installer
+C:\Python25\python make-installer.py
 C:\Progra~1\nsis\makensis eclass-builder.nsi
