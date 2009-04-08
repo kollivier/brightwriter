acad2f6d605909b8b6c26dad53df30fcac3f4f2f
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2009-04-08 18:41:38 +0000

    Detab.

--- trunk/eclass_builder/conman/xml_settings.py
+++ trunk/eclass_builder/conman/xml_settings.py
@@ -6,104 +6,104 @@
 
 USE_MINIDOM=0
 try:
-	from xml.dom.ext.reader.Sax import FromXmlFile
+    from xml.dom.ext.reader.Sax import FromXmlFile
 except:
-	USE_MINIDOM=1
+    USE_MINIDOM=1
 
 if USE_MINIDOM:
-	from xml.dom import minidom
+    from xml.dom import minidom
 
 import sys
 
 class XMLSettings:
-	def __init__(self):
-		self.settings = {}
-		self.filename = ""
+    def __init__(self):
+        self.settings = {}
+        self.filename = ""
 
-	def __getitem__(self, key):
-		if not self.settings.has_key(key):
-			self.settings[key] = "" 
-		return self.settings[key]
+    def __getitem__(self, key):
+        if not self.settings.has_key(key):
+            self.settings[key] = "" 
+        return self.settings[key]
 
-	def __setitem__(self, key, value):
-		self.settings[key] = value
+    def __setitem__(self, key, value):
+        self.settings[key] = value
 
-	def __delitem__(self, key):
-		del self.settings[key]
+    def __delitem__(self, key):
+        del self.settings[key]
 
-	def __getslice__(self, low, high):
-		return self.settings[low:high]
+    def __getslice__(self, low, high):
+        return self.settings[low:high]
 
-	def __setslice__(self, low, high, seq):
-		self.settings[low:high] = seq
+    def __setslice__(self, low, high, seq):
+        self.settings[low:high] = seq
 
-	def __delslice__(self, low, high):
-		del self.settings[low:high]
+    def __delslice__(self, low, high):
+        del self.settings[low:high]
 
-	def __repr__(self):
-		text = ""
-		for key in self.settings.keys():
-			text = text + key + "=" + str(self.settings[key]) + "\n"
-		return text
-		
-	def keys(self):
-		return self.settings.keys()
+    def __repr__(self):
+        text = ""
+        for key in self.settings.keys():
+            text = text + key + "=" + str(self.settings[key]) + "\n"
+        return text
+        
+    def keys(self):
+        return self.settings.keys()
 
-	def Add(self, key, value):
-		self.settings[key] = value
+    def Add(self, key, value):
+        self.settings[key] = value
 
-	def Remove(self, key):
-		del self.settings[key]
+    def Remove(self, key):
+        del self.settings[key]
 
-	def LoadFromXML(self, filename):
-		self.settings = {}
-		self.filename = filename
-		if USE_MINIDOM:
-			doc = minidom.parse(open(filename))
-		else:
-			doc = FromXmlFile(filename)
+    def LoadFromXML(self, filename):
+        self.settings = {}
+        self.filename = filename
+        if USE_MINIDOM:
+            doc = minidom.parse(open(filename))
+        else:
+            doc = FromXmlFile(filename)
 
-		settings = doc.getElementsByTagName("Setting")
-		for item in settings:
-			if item.attributes:
-				myname = ""
-				myvalue = ""
-				for i in range(0, len(item.attributes)):
-					attr = item.attributes.item(i)
-					if attr.name == "name":
-						myname = attr.value
-					elif attr.name == "value":
-						myvalue = attr.value
-				self.settings[myname] = myvalue
+        settings = doc.getElementsByTagName("Setting")
+        for item in settings:
+            if item.attributes:
+                myname = ""
+                myvalue = ""
+                for i in range(0, len(item.attributes)):
+                    attr = item.attributes.item(i)
+                    if attr.name == "name":
+                        myname = attr.value
+                    elif attr.name == "value":
+                        myvalue = attr.value
+                self.settings[myname] = myvalue
 
-	def SaveAsXML(self, filename):
-		XML = """<?xml version="1.0" encoding="utf-8"?>\n<Settings>"""
-		doc = minidom.Document()
-		root = doc.createElement("Settings")
-		
-		for key in self.settings.keys():
-			setting = doc.createElement("Setting")
-			setting.setAttribute("name", key)
-			setting.setAttribute("value", self.settings[key])
-			root.appendChild(setting)
-		
-		doc.appendChild(root)
-		
-		import codecs
-		data = doc.toprettyxml("\t", encoding="utf-8")
-		myfile = open(filename, "w")
-		myfile.write(codecs.BOM_UTF8)
-		myfile.write(data)
-		myfile.close()
-		return None
+    def SaveAsXML(self, filename):
+        XML = """<?xml version="1.0" encoding="utf-8"?>\n<Settings>"""
+        doc = minidom.Document()
+        root = doc.createElement("Settings")
+        
+        for key in self.settings.keys():
+            setting = doc.createElement("Setting")
+            setting.setAttribute("name", key)
+            setting.setAttribute("value", self.settings[key])
+            root.appendChild(setting)
+        
+        doc.appendChild(root)
+        
+        import codecs
+        data = doc.toprettyxml("\t", encoding="utf-8")
+        myfile = open(filename, "w")
+        myfile.write(codecs.BOM_UTF8)
+        myfile.write(data)
+        myfile.close()
+        return None
 
 def Test():
-	settings = XMLSettings()
-	settings.LoadFromXML("xmltest1.xml")
-	print `settings`
+    settings = XMLSettings()
+    settings.LoadFromXML("xmltest1.xml")
+    print `settings`
 
 if __name__ == "__main__":
-	if len(sys.argv) > 1:
-		if sys.argv[1] == "-t":
-			Test()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "-t":
+            Test()
 
