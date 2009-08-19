98e506a94d9005e9d91fbce2a734307df5c4420b
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2009-08-19 21:42:22 +0000

    Temp fix for an issue where wx.StandardPaths.GetUserDataDir returns an empty string on Windows.

--- trunk/eclass_builder/guiutils.py
+++ trunk/eclass_builder/guiutils.py
@@ -121,7 +121,7 @@ def getAppDataDir():
 	dir = ""
 	if wx.GetApp():
 		dir = wx.StandardPaths.Get().GetUserDataDir()
-		if not os.path.exists(dir):
+		if dir != "" and not os.path.exists(dir):
 			os.mkdir(dir)
 	
 	return dir
