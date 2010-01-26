bea646d2818cff238bb04f71a64492ea213bb424
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2010-01-26 22:06:36 +0000

    Use the python set by the environment.

--- trunk/eclass_builder/installer/build-mac.sh
+++ trunk/eclass_builder/installer/build-mac.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 if [ "$skipmac" != "yes" ]; then
      BUILD_TYPE=""
-     PYTHON="/usr/local/bin/python2.5"
+     PYTHON="python"
      if [ "$IS_INTEL" == "yes" ]; then
          MAC_HOST=$MAC_HOST_INTEL
          BUILD_TYPE=""
