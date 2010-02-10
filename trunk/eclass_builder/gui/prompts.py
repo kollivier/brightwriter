f54e748b49a62e167a1575edb31b675cb62d641b
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2010-02-10 22:48:30 +0000

    Switch over to Python logs completely.

--- trunk/eclass_builder/gui/prompts.py
+++ trunk/eclass_builder/gui/prompts.py
@@ -1,20 +1,22 @@
 import wx
-import errors
+
+import logging
+log = logging.getLogger('EClass')
 
 class GUIErrorCallbacks:
     def displayError(self, message, title=_("EClass.Builder Error")):
         wx.MessageBox(message, style=wx.OK|wx.ICON_ERROR)
-        if errors.appErrorLog:
-            errors.appErrorLog.write(message)
+        if log:
+            log.error(message)
             
     def displayWarning(self, message, title=_("EClass.Builder Warning")):
         wx.MessageBox(message, style=wx.OK|wx.ICON_EXCLAMATION)
-        if errors.appErrorLog:
-            errors.appErrorLog.write(message)
+        if log:
+            log.warning(message)
             
     def displayInformation(self, message, title=_("EClass.Builder Message")):
         wx.MessageBox(message, style=wx.OK|wx.ICON_INFORMATION)
-        if errors.appErrorLog:
-            errors.appErrorLog.write(message)            
+        if log:
+            log.info(message)            
 
 errorPrompts = GUIErrorCallbacks()
