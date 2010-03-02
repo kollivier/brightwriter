cf04908b5f2ab52792f25e05130c19cb8db45a07
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2010-03-02 18:05:14 +0000

    Fixes to the Windows build script.

--- eclass_builder/installer/make_installer.bat
+++ eclass_builder/installer/make_installer.bat
@@ -1,9 +1,7 @@
 call "%VS90COMNTOOLS%vsvars32.bat"
 C:\Python26\python ..\updateVersion.py %1
 nmake -f makefile.vc
-cd ..\autorun\loader
-nmake -f loader.mak
-cd ..\..
+cd ..
 C:\Python26\python runTests.py
 cd installer
 C:\Python26\python make-installer.py
