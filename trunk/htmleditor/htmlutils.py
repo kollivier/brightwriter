e674281a89821b97baffc1898abded436f137b3a
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2010-08-23 22:46:37 +0000

    The word-2000 flag actually leaves tags like <o:p> in the document, but thankfully clean and bare clean them up properly instead.

--- trunk/htmleditor/htmlutils.py
+++ trunk/htmleditor/htmlutils.py
@@ -80,13 +80,13 @@ def cleanUpHTML(html, options=None):
     tidylib.BASE_OPTIONS = {}
 
     default_options = { 
-                        "Word-2000" : 1,
                         "force-output" : 1,
                         "output-xhtml" : 1,
                         "doctype" : "strict",
                         "drop-empty-paras": 1,
                         "output-encoding" : "utf8",
-                        "clean": 1
+                        "clean": 1,
+                        "bare": 1
                        }
     if options:
         default_options.extend(options)
