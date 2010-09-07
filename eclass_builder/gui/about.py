764e564e8ff8845cb33bbae42774f7f667e735a3
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2010-09-07 20:09:31 +0000

    Switch to using FatCow icons as they look sharper, remove old, unused icons, and update the About dialog to look more native and have proper attribution.

--- eclass_builder/gui/about.py
+++ eclass_builder/gui/about.py
@@ -1,26 +1,48 @@
 import string, sys, os
 import wx
-import wxbrowser
-import settings
+import wx.lib.sized_controls as sc
+import wx.lib.hyperlink as hl
+import version
 
-class EClassAboutDialog(wx.Dialog):
-	def __init__(self, parent):
-		wx.Dialog.__init__ (self, parent, -1, _("About EClass.Builder"), wx.Point(100,100),wx.Size(460,400), wx.DIALOG_MODAL|wx.DEFAULT_DIALOG_STYLE)
-		self.parent = parent
-		self.browser = wxbrowser.wxBrowser(self, -1)
-		self.browser.LoadPage(os.path.join(settings.AppDir,"about", settings.LangDirName, "about_eclass.html"))
-		
-		self.btnOK = wx.Button(self,wx.ID_OK,_("OK"))
-		self.btnOK.SetDefault()
-		self.mysizer = wx.BoxSizer(wx.VERTICAL)
-		self.mysizer.Add(self.browser.browser, 1, wx.EXPAND|wx.ALL, 4)
-		self.mysizer.Add(self.btnOK, 0, wx.ALIGN_CENTER|wx.ALL, 6)			
+rootdir = os.path.join(os.path.dirname(__file__), "..")
 
-		self.SetAutoLayout(True)
-		self.SetSizer(self.mysizer)
-		self.Layout()
+class EClassAboutDialog(sc.SizedDialog):
+    def __init__(self, parent):
+        sc.SizedDialog.__init__ (self, parent, -1, _("About EClass.Builder"), wx.Point(100,100))
+        self.parent = parent
+        
+        panel = self.GetContentsPane()
+        
+        icon = wx.StaticBitmap(panel, -1, wx.Bitmap(os.path.join(rootdir, "icons", "eclass_builder.png")))
+        icon.SetSizerProps(halign="center")
+        
+        app_name = wx.StaticText(panel, -1, wx.GetApp().GetAppName())
+        app_name.SetSizerProps(halign="center")
+        name_font = app_name.GetFont()
+        name_font.SetWeight(wx.FONTWEIGHT_BOLD)
+        app_name.SetFont(name_font)
 
-		wx.EVT_BUTTON(self.btnOK, self.btnOK.GetId(), self.btnOKClicked)	
+        app_version = wx.StaticText(panel, -1, version.asString())
+        app_version.SetSizerProps(halign="center")
+        
+        app_copyright = wx.StaticText(panel, -1, "Copyright 2002-2010 Tulane University")
+        app_copyright.SetSizerProps(halign="center")
+        
+        app_license = wx.StaticText(panel, -1, "This program is BSD licensed")
+        app_license.SetSizerProps(halign="center")
+        
+        acknowledgements = wx.StaticText(panel, -1, "Thanks and Acknowledgements")
+        acknowledgements.SetSizerProps(halign="center")
+        ack_font = acknowledgements.GetFont()
+        ack_font.SetWeight(wx.FONTWEIGHT_BOLD)
+        acknowledgements.SetFont(ack_font)
+        
+        wxpy_link = hl.HyperLinkCtrl(panel, -1, "Built with wxPython", URL="http://www.wxpython.org")
+        wxpy_link.SetSizerProps(halign="center")
+        
+        fatcow_link = hl.HyperLinkCtrl(panel, -1, "EClass.Builder uses FatCow free icons", URL="http://www.fatcow.com/free-icons/")
+        fatcow_link.SetSizerProps(halign="center")
 
-	def btnOKClicked(self, event):
-		self.EndModal(wx.ID_OK)
+        program_icon = hl.HyperLinkCtrl(panel, -1, "EClass.Builder icon created from iconaholic.com icons", URL="http://www.iconaholic.com/")
+
+        self.Fit()
