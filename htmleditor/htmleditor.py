d104879dedd209ee760da006d6bc2e37565d281e
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2010-09-22 16:24:46 +0000

    Move the HTML source editing delegate to its own file.

--- htmleditor/htmleditor.py
+++ htmleditor/htmleditor.py
@@ -21,6 +21,8 @@ import htmledit.htmlattrs as htmlattrs
 import editordelegate
 import edittoolbar
 import htmlutils
+import sourcedelegate
+
 import aboutdialog
 import cleanhtmldialog
 
@@ -51,94 +53,6 @@ def getMimeTypeForHTML(html):
     if html.find("//W3C//DTD XHTML") != -1:
         mimetype = 'application/xhtml+xml'
     return mimetype
-
-class STCFindReplaceController(wx.EvtHandler):
-    '''
-    This class controls Find and Replace behaviors for wxSTC, e.g. adding
-    "move to selection" behavior and setting up the keyboard shortcuts.
-    '''
-    def __init__(self, stc, *args, **kwargs):
-        wx.EvtHandler.__init__(self, *args, **kwargs)
-        self.stc = stc
-        self.searchText = None
-        self.lastFindResult = -1
-        self.BindEvents()
-        
-    def OnKeyDown(self, event):
-        is_search = event.MetaDown() and event.KeyCode == 'G'
-        
-        if self.searchText and is_search:
-            self.DoInlineSearch(self.searchText, next=True)
-        else:
-            event.Skip()
-        
-    def DoInlineSearch(self, text, next=False, back=False):
-        self.searchText = text
-        startPos = 0
-        if self.lastFindResult > 0 and next:
-            startPos = self.lastFindResult + 1
-
-        self.lastFindResult = self.stc.FindText(0, self.stc.GetLength(), text)
-        if self.lastFindResult != -1:
-            self.stc.SetCurrentPos(self.lastFindResult)
-            self.stc.EnsureCaretVisible()
-            self.stc.SetSelectionStart(self.lastFindResult)
-            self.stc.SetSelectionEnd(self.lastFindResult + len(text))
-                
-    def BindEvents(self):
-        self.stc.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
-
-class HTMLSourceEditorDelegate(wx.EvtHandler):
-    def __init__(self, source, *args, **kwargs):
-        wx.EvtHandler.__init__(self, *args, **kwargs)
-        self.source = source
-        self.sourceFindHandler = STCFindReplaceController(self.source)
-        self.searchId = None
-        Publisher().subscribe(self.OnDoSearch, ('search', 'text', 'changed'))
-        
-    def RegisterHandlers(self, event=None):       
-        app = wx.GetApp()
-        app.AddHandlerForID(ID_UNDO, self.OnUndo)
-        app.AddHandlerForID(ID_REDO, self.OnRedo)
-        app.AddHandlerForID(ID_SELECTALL, self.OnSelectAll)
-        app.AddHandlerForID(ID_SELECTNONE, self.OnSelectNone)
-        
-        search = self.source.FindWindowByName("searchctrl")
-        if search:
-            self.searchId = search.GetId()
-            app.AddHandlerForID(self.searchId, self.OnDoSearch)
-        
-    def RemoveHandlers(self, event=None):
-        app = wx.GetApp()
-        app.RemoveHandlerForID(ID_UNDO)
-        app.RemoveHandlerForID(ID_REDO)
-        app.RemoveHandlerForID(ID_SELECTALL)
-        app.RemoveHandlerForID(ID_SELECTNONE)
-        
-    def OnUndo(self, event):
-        self.source.Undo()
-        
-    def OnRedo(self, event):
-        self.source.Redo()
-        
-    def OnDoSearch(self, message):
-        if wx.GetTopLevelParent(self.source).IsActive():
-            self.sourceFindHandler.DoInlineSearch(message.data)
-
-    def OnSelectAll(self, evt):
-        self.source.SelectAll()
-
-    def OnSelectNone(self, evt):
-        self.source.SetSelection(-1, self.source.GetCurrentPos())    
-
-    def OnCut(self, evt):
-        self.source.Cut()
-
-    def OnCopy(self, evt):
-        self.source.Copy()
-
-    def OnPaste(self, evt):
-        self.source.Paste()
 
 class EditorFrame (sc.SizedFrame):
     def __init__(self, parent, filename, pos=wx.DefaultPosition, size=(660,400)):
@@ -282,7 +196,7 @@ class EditorFrame (sc.SizedFrame):
         self.source.StyleSetSpec(wx.stc.STC_H_VALUE, "fore:#009900")
         self.source.SetProperty("fold.html", "1")
 
-        self.sourceDelegate = HTMLSourceEditorDelegate(self.source)
+        self.sourceDelegate = sourcedelegate.HTMLSourceEditorDelegate(self.source)
 
         accelerators = wx.AcceleratorTable([(wx.ACCEL_CTRL, ord('B'), ID_BOLD),(wx.ACCEL_CTRL, ord('I'), ID_ITALIC), (wx.ACCEL_CTRL, ord('U'), ID_UNDERLINE), (wx.ACCEL_CTRL, ord('S'), ID_SAVE)]) 
         self.SetAcceleratorTable(accelerators)
