18a99414fec8ca89cfafabf3accd5c83acc44e1f
kollivier <kollivier@a3b1a89e-1326-0410-aa8b-de5a7c427257>
2010-10-14 19:24:48 +0000

    Update the command line exporter to work with the new version.

--- eclass_builder/publish.py
+++ eclass_builder/publish.py
@@ -1,5 +1,19 @@
 # Script for publishing EClass from the command line.
 import sys, os
+
+rootdir = os.path.abspath(sys.path[0])
+if not os.path.isdir(rootdir):
+    rootdir = os.path.dirname(rootdir)
+    
+localedir = os.path.join(rootdir, 'locale')
+import gettext
+gettext.install('eclass', localedir)
+lang_dict = {
+            "en": gettext.translation('eclass', localedir, languages=['en']), 
+            "es": gettext.translation('eclass', localedir, languages=['es']),
+            "fr": gettext.translation('eclass', localedir, languages=['fr'])
+            }
+
 import settings
 import conman.xml_settings as xml_settings
 import appdata
@@ -7,71 +21,60 @@ import ims
 import ims.utils
 import eclassutils
 import eclass_convert
+import fileutils
+import utils
 
-rootdir = os.path.abspath(sys.path[0])
-if not os.path.isdir(rootdir):
-	rootdir = os.path.dirname(rootdir)
+import themes
 
 settings.AppDir = rootdir
-	
-localedir = os.path.join(rootdir, 'locale')
-import gettext
-gettext.install('eclass', localedir)
-lang_dict = {
-			"en": gettext.translation('eclass', localedir, languages=['en']), 
-			"es": gettext.translation('eclass', localedir, languages=['es']),
-			"fr": gettext.translation('eclass', localedir, languages=['fr'])
-			}
 
-import conman
-import plugins
-plugins.LoadPlugins()
-import themes
-	
-class EClassPublisher:
-    def __init__(self, filename, pubdir, format="html"):
-        converter = eclass_convert.EClassIMSConverter(filename)
+class CLFileCopyCallback:
+    def fileChanged(self, filename):
+        print "Copying %s" % filename
+
+class EClassExporter:
+    def __init__(self, filename, pubdir=None, format="html"):
         self.imscp = None
-        if converter.IsEClass():
-            # TODO: detect if there are non-ascii characters,
-            # and prompt the user for language to convert from
-            self.imscp = converter.ConvertToIMSContentPackage()
-        else:
-            self.imscp = ims.contentpackage.ContentPackage()
-            self.imscp.loadFromXML(filename)
+        self.imscp = ims.contentpackage.ContentPackage()
+        self.imscp.loadFromXML(filename)
+        self.pubdir = pubdir
         appdata.currentPackage = self.imscp
         
         settings.ProjectDir = os.path.dirname(filename)
         # TODO: We should store the project as a global rather than its settings
         settings.ProjectSettings = xml_settings.XMLSettings()
         settingsfile = os.path.join(settings.ProjectDir, "settings.xml")
-        theme = ""
-        if os.path.exists(settingsfile):
-            settings.ProjectSettings.LoadFromXML(settingsfile)
-                    
-            theme = settings.ProjectSettings["Theme"]
 
-        if theme == "":
-            theme = "Simple"
-        
-        themeList = themes.ThemeList(os.path.join(rootdir, "themes"))
-        self.currentTheme = themeList.FindTheme(theme)
-        settings.ProjectDir = os.path.dirname(filename)
-        publisher = self.currentTheme.HTMLPublisher(self, dir=pubdir)
-        publisher.Publish()
-        
-    def PublishPage(self, imsitem):
-        if imsitem != None:
-            filename = eclassutils.getEditableFileForIMSItem(self.imscp, imsitem)
-            publisher = plugins.GetPublisherForFilename(filename)
-            if publisher:
-                publisher.Publish(self, imsitem, settings.ProjectDir)
+    def CopyWebFiles(self):
+        utils.CreateJoustJavascript(self.imscp.organizations[0].items[0], self.pubdir)
+        utils.CreateiPhoneNavigation(self.imscp.organizations[0].items[0], self.pubdir)
+        self.currentTheme = themes.FindTheme(settings.ProjectSettings["Theme"])
+        if self.currentTheme:
+            self.currentTheme.HTMLPublisher(self, self.pubdir).CopySupportFiles()
+
+    def ExportToWeb(self):
+        if not self.pubdir:
+            if settings.ProjectSettings["WebSaveDir"] == '':
+                print "ERROR: No output directory specified. Exiting."
+                sys.exit(1)
+            else:
+                self.pubdir = settings.ProjectSettings["WebSaveDir"]
+        callback = CLFileCopyCallback()
+        fileutils.CopyFiles(settings.ProjectDir, self.pubdir, 1, callback)
+        self.CopyWebFiles()
     
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    from optparse import OptionParser
+    parser = OptionParser(usage="usage: %prog [options] eclass_dir")
+    parser.add_option("-o", "--output-directory", dest="output_dir",
+                  help="Directory to store exported files", default=None)
+
+    (options, args) = parser.parse_args()
+
+    if len(args) < 1:
         print "You must specify an EClass to publish. Exiting."
         
-    eclass = sys.argv[1]
+    eclass = args[0]
     if not os.path.isfile(eclass):
         eclass = os.path.join(eclass, "imsmanifest.xml")
         if not os.path.exists(eclass):
@@ -79,6 +82,7 @@ if __name__ == "__main__":
             sys.exit(1)
     
     # publish to the 
-    EClassPublisher(eclass, os.path.dirname(eclass))
+    exporter = EClassExporter(eclass, options.output_dir)
+    exporter.ExportToWeb()
     
     print "Finished publishing %s!" % eclass
