4dea18b9c085f5b7454998914fd76dbb04150f4a
kollivier <kevino@theolliviers.com>
2020-11-27 10:00:38 -0800

    Remove EClass Library code as there are no plans to continue it.

--- src/library/gui/Indexes.py
+++
@@ -1,7 +0,0 @@
-import wx
-import wx.lib.sized_controls as sc
-
-class IndexList(sc.SizedPanel):
-    def __init__(self, parent, id, indexList=[]):
-        sc.SizedPanel.__init__(self, parent, id)
-        
